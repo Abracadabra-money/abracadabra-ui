51bfdabd825b52feac1a94ae479611094da8fb1c
Andrey <onipchenko.andrey.90@gmail.com>
2022-02-15 14:39:12 +0200

    refactoring myposition component

--- src/views/MyPositions.vue
+++ src/views/MyPositions.vue
@@ -8,7 +8,7 @@
       </div>
 
       <div class="underline">
-        <PositionType @changeType="changeType" />
+        <MasterContractsTab @changeType="changeType" />
       </div>
 
       <div class="my-position">
@@ -57,46 +57,15 @@ const MyPositionItem = () =>
   import("@/components/myPosition/MyPositionItem.vue");
 
 const StableInfo = () => import("@/components/borrow/StableInfo");
-const PositionType = () => import("@/components/myPosition/PositionType");
+const MasterContractsTab = () =>
+  import("@/components/myPosition/MasterContractsTab");
 const Tabs = () => import("@/components/myPosition/Tabs");
 const Repay = () => import("@/components/myPosition/Repay");
 const Collateral = () => import("@/components/myPosition/Collateral");
 const Deleverage = () => import("@/components/myPosition/Deleverage");
 
-import ethIcon from "@/assets/images/networks/ethereum-icon.svg";
-import fantomIcon from "@/assets/images/networks/fantom-icon.svg";
-import polygonIcon from "@/assets/images/networks/polygon-icon.svg";
-import binanceIcon from "@/assets/images/networks/binance-icon.svg";
-import avalancheIcon from "@/assets/images/networks/avalanche-icon.png";
-import arbitrumIcon from "@/assets/images/networks/arbitrum-icon.svg";
-
 export default {
   data: () => ({
-    selectedNetwork: null,
-    isListOpened: false,
-    networks: [
-      { name: "ERC-20", icon: ethIcon },
-      {
-        name: "Fantom",
-        icon: fantomIcon,
-      },
-      {
-        name: "BSC",
-        icon: binanceIcon,
-      },
-      {
-        name: "AVAX",
-        icon: avalancheIcon,
-      },
-      {
-        name: "AETH",
-        icon: arbitrumIcon,
-      },
-      {
-        name: "MATIC",
-        icon: polygonIcon,
-      },
-    ],
     myPositions: [
       {
         icon: require("@/assets/images/myposition/ETH.svg"),
@@ -123,20 +92,9 @@ export default {
         total: "32,504.49898",
       },
     ],
-    lineHeight: 50,
-    linesGap: 16,
-    activeType: null,
     activeTab: "repay",
     activePosition: null,
   }),
-  computed: {
-    listMaxHeight() {
-      const lines = Math.ceil(this.networks.length / 4);
-      return this.isListOpened
-        ? lines * this.lineHeight + (lines - 1) * this.linesGap
-        : this.lineHeight;
-    },
-  },
 
   methods: {
     enterPosition(pos) {
@@ -158,7 +116,7 @@ export default {
     NetworksList,
     MyPositionItem,
     StableInfo,
-    PositionType,
+    MasterContractsTab,
     Tabs,
     Repay,
     Collateral,
@@ -185,50 +143,6 @@ export default {
   overflow: hidden;
 }
 
-.networks-wrap {
-  position: relative;
-}
-
-.networks {
-  position: relative;
-  left: -16px;
-  width: calc(100% + 32px);
-  margin-top: 10px;
-  padding-bottom: 30px;
-  padding-left: 16px;
-  padding-right: 16px;
-  overflow-y: hidden;
-  overflow-x: scroll;
-}
-
-.list {
-  display: grid;
-  grid-auto-flow: column;
-  grid-gap: 16px;
-  max-height: 50px;
-  transition: height 0.2s ease-out;
-  width: max-content;
-}
-
-.networks-arrow-btn-pressed {
-  transform: rotate(180deg);
-}
-
-.networks-arrow-btn {
-  position: absolute;
-  right: 10px;
-  top: -28px;
-  border: none;
-  background-color: transparent;
-  cursor: pointer;
-  transition: transform 0.2s;
-  display: none;
-}
-
-.networks-arrow-btn-image {
-  width: 11px;
-}
-
 .underline {
   border-bottom: 1px solid rgba(255, 255, 255, 0.1);
 }
@@ -245,8 +159,6 @@ export default {
   line-height: 24px;
   margin-bottom: 20px;
 }
-
-// stable
 
 .stable {
   padding: 30px;
@@ -280,24 +192,6 @@ export default {
 
   .choose-chain {
     padding: 30px;
-  }
-
-  .list {
-    grid-template-columns: repeat(4, 1fr);
-    grid-auto-flow: row;
-    overflow: hidden;
-    max-height: none;
-  }
-
-  .networks-arrow-btn {
-    display: block;
-  }
-
-  .networks {
-    position: static;
-    width: auto;
-    overflow-x: hidden;
-    padding-left: 0;
   }
 }
 </style>
