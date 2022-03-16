ed35779fb06ccb988ddd3a49d7b40ebc17877da8
valentin.belousov <belousowork@gmail.com>
2022-03-16 08:21:59 +0200

    Renamed store/modules/pools to store/modules/farms

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,7 @@ import networks from "@/store/modules/networks";
 import notifications from "@/store/modules/notifications";
 import bridge from "@/store/modules/bridge";
 import popups from "@/store/modules/popups";
-import pools from "@/store/modules/pools";
+import pools from "@/store/modules/farms";
 
 Vue.use(Vuex);
 
