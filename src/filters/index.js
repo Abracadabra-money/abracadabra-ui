70fd510a8fa0894216b0217d473c46813ae442d6
0xhardrefresh <0xhardrefresh@protonmail.com>
2022-05-03 16:44:31 +0300

    update filters

--- src/filters/index.js
+++ src/filters/index.js
@@ -1,76 +1,74 @@
-const isValidNumber = (num) => !isNaN(num) && num !== 0;
+const formatUSD = (value) => {
+  if (isNaN(Number(value)) || +value === 0 || +value < 0.0001) return "$ 0.0";
+  const decimals = +value < 1 ? 4 : 2;
 
-const customToFixed = (value, fixed = 2) => {
-  let [beforePoint, afterPoint = ""] = value.split(".");
-  afterPoint = afterPoint.slice(0, fixed);
+  const formatAmount = +parseFloat(value).toFixed(decimals);
 
-  if (afterPoint) {
-    afterPoint = afterPoint.padEnd(fixed, "0");
-  }
-
-  return `${beforePoint}${afterPoint ? `.${afterPoint}` : ""}`;
+  return `$ ${formatAmount.toLocaleString("en-US", { currency: "USD" })}`;
 };
 
-const formatAfterPoint = (num, maxFixed = 4) => {
-  if (num >= 1) {
-    const numArray = customToFixed(num.toString()).split(".");
-    numArray[0] = numArray[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    return numArray.join(".");
-  }
+const formatLargeSum = (value) => {
+  if (isNaN(Number(value)) || Number(value) < 1) return "0";
 
-  const fixedMinNum = customToFixed(num.toString());
-  const parsedNum = Number(customToFixed(num.toString(), maxFixed)).toString();
-  return fixedMinNum.length > parsedNum.length ? fixedMinNum : parsedNum;
+  const lookup = [
+    { value: 0, symbol: "" },
+    { value: 1, symbol: "" },
+    { value: 1e3, symbol: "k" },
+    { value: 1e6, symbol: "M" },
+  ];
+  const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+  let item = lookup.reverse().find((item) => parseFloat(value) >= item.value);
+  return `${(parseFloat(value) / item.value).toFixed(2).replace(rx, "$1")}${
+    item.symbol
+  }`;
+};
+
+const formatToFixed = (value, fixed) => {
+  if (isNaN(Number(value))) return "0";
+
+  const itsNumber = typeof value === "number";
+  const maxFixedValue = itsNumber ? parseFloat(value).toFixed(20) : value;
+  //eslint-disable-next-line no-useless-escape
+  let re = new RegExp(`^-?\\d+(?:\.\\d{0,` + (fixed || -1) + `})?`);
+
+  const parsedValue = maxFixedValue.match(re)[0];
+
+  if (+parsedValue === 0) return "0";
+
+  const removedZero = parsedValue.replace(/0*$/, "");
+  const removedDot = removedZero.endsWith(".")
+    ? removedZero.slice(0, -1)
+    : removedZero;
+  return removedDot;
+};
+
+const formatTokenBalance = (value) => {
+  if (isNaN(Number(value)) || +value === 0) return "0.0";
+
+  if (+value >= 1) return formatToFixed(value, 4);
+  if (+value > 0.0000001) return formatToFixed(value, 6);
+
+  const stringValue = formatToFixed(value, 18);
+
+  const start = stringValue.slice(0, 1);
+  const endToParse = `${stringValue.slice(1)}`;
+
+  const persedEnd = endToParse.replace(
+    new RegExp(
+      // eslint-disable-next-line no-useless-escape
+      `(?<=\.)[0]*`
+    ),
+    ""
+  );
+
+  return `${start}..${persedEnd.slice(0, 5)}`;
 };
 
 const filters = {
-  usdFormat: function (value) {
-    let resString = "0.0";
-    const num = Number(value);
-    if (isValidNumber(num) && num >= 0.0001) {
-      resString = formatAfterPoint(num);
-    }
-    return `$ ${resString}`;
-  },
-  tokenBalanceFormat: function (value) {
-    let resString = "0.0";
-    const num = Number(value);
-    if (isValidNumber(num)) {
-      const numArray = String(value).split(".");
-      if (numArray[1] && numArray[1].slice(0, 6) === "000000") {
-        let startPos = 0;
-        for (const char of numArray[1]) {
-          if (char !== "0") break;
-          startPos++;
-        }
-        resString = `${numArray[0]}.0...${numArray[1].slice(
-          startPos,
-          startPos + 5
-        )}`;
-      } else {
-        resString = formatAfterPoint(num, 6);
-      }
-    }
-    return resString;
-  },
-  largeSumFormat: function (value) {
-    if (isNaN(Number(value)) || Number(value) < 1) return "0";
-
-    const lookup = [
-      { value: 0, symbol: "" },
-      { value: 1, symbol: "" },
-      { value: 1e3, symbol: "k" },
-      { value: 1e6, symbol: "M" },
-    ];
-    const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
-    let item = lookup.reverse().find((item) => parseFloat(value) >= item.value);
-    return `${(parseFloat(value) / item.value).toFixed(2).replace(rx, "$1")}${
-      item.symbol
-    }`;
-  },
-  appToFixed: function (value, fixed) {
-    return customToFixed(String(value), fixed);
-  },
+  formatUSD,
+  formatTokenBalance,
+  formatLargeSum,
+  formatToFixed,
 };
 
 export default filters;
