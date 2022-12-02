5ce74fd875a1ae95e6f93f2b5af819c848bbf1df
Andrey <onipchenko.andrey.90@gmail.com>
2022-12-02 18:08:09 +0200

    added strategy link

--- src/utils/borrowPools/arbitrumPools.js
+++ src/utils/borrowPools/arbitrumPools.js
@@ -67,7 +67,8 @@ export default [
     isSwappersActive: false,
     cauldronSettings: {
       isDegenBox: true,
-      strategyLink: "#",
+      strategyLink:
+        "https://mirror.xyz/0x5744b051845B62D6f5B6Db095cc428bCbBBAc6F9/u0vFLaZaHrstsu7V-pcs58ufFm7s6Sdw9coKgOfEkXM",
       isDepreciated: false,
       acceptUseDefaultBalance: false,
       healthMultiplier: 1,
