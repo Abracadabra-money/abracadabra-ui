23c087305f811ecbd1e6ea16ba7311f40c14d461
Leonardo <leonardosalazard@gmail.com>
2022-12-28 12:15:31 -0400

    refactor(store): remove networks module

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 import connectProvider from "@/store/modules/connectProvider";
-import networks from "@/store/modules/networks";
 import notifications from "@/store/modules/notifications";
 import bridge from "@/store/modules/bridge";
 import popups from "@/store/modules/popups";
@@ -15,7 +14,6 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   modules: {
     connectProvider,
-    networks,
     notifications,
     bridge,
     popups,
