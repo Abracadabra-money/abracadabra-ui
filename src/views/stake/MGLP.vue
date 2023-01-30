f10096bb8ac421ff2698f4858f123b3d78c9ecfa
0xhardrefresh <0xhardrefresh@protonmail.com>
2023-01-30 19:15:21 +0200

    fix

--- src/views/stake/MGLP.vue
+++ src/views/stake/MGLP.vue
@@ -227,7 +227,7 @@
           tedious work! Simply deposit your sGLP into MagicGLP and let it do its
           magic!
           <br />
-          Note: A protocol 1% fee it taken on the profits.
+          Note: A 1% protocol fee is taken on the profits.
         </p>
         <div class="links-wrap">
           <a
