5432588e355025df15081cbb7da6a704d6ab17ae
roxx3r <96884073+roxx3r@users.noreply.github.com>
2023-02-14 23:51:16 -0600

    comment out geo gate

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ const AppHeader = () => import("@/components/app/AppHeader");
 const PopupsWrapper = () => import("@/components/popups/PopupsWrapper");
 const Banner = () => import("@/components/ui/Banner");
 const SkullBanner = () => import("@/components/ui/SkullBanner");
-import axios from "axios";
+// import axios from "axios";
 
 export default {
   computed: {
@@ -30,16 +30,16 @@ export default {
     }),
   },
 
-  async beforeCreate() {
-    const response = await axios.get(
-      `https://ipwhois.pro/?key=${process.env.VUE_APP_IPWHOIS_API_KEY}`
-    );
-
-    const location = response.data.country;
-
-    if (location === "United States")
-      document.location.href = "https://abracadabra.money/location";
-  },
+//  async beforeCreate() {
+//    const response = await axios.get(
+//      `https://ipwhois.pro/?key=${process.env.VUE_APP_IPWHOIS_API_KEY}`
+//    );
+//
+//    const location = response.data.country;
+//
+//    if (location === "United States")
+//      document.location.href = "https://abracadabra.money/location";
+//  },
 
   components: {
     AppHeader,
