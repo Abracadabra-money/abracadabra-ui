3a6c28b8f84d007e5af44d55b07195bbb606437f
roxx3r <roxx3r@st.lk>
2023-02-20 14:57:54 -0600

    ignore location check

--- src/App.vue
+++ src/App.vue
@@ -59,6 +59,8 @@ export default {
   },
 
   async beforeCreate() {
+    return
+
     const location = await axios.get(
       `https://ipwhois.pro/?key=${process.env.VUE_APP_IPWHOIS_API_KEY}`
     );
@@ -67,7 +69,7 @@ export default {
       this.country.includes(location.data.country) ||
       this.region.includes(location.data.region)
     )
-      // document.location.href = "https://abracadabra.money/location";
+      document.location.href = "https://abracadabra.money/location";
   },
 
   components: {
