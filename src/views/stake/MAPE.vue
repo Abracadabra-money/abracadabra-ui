4cdaf42450e0bc47ab115f5617050af2ab408b82
0xhardrefresh <98540076+0xhardrefresh@users.noreply.github.com>
2023-02-21 04:05:46 +0200

    mAPE: add temporary reward placeholder  (#65)
    * init
    
    * init

--- src/views/stake/MAPE.vue
+++ src/views/stake/MAPE.vue
@@ -221,11 +221,14 @@
                 <span>APE</span>
               </div>
               <div class="info-balance">
+                <span class="info-value">Gathering Data</span>
+              </div>
+              <!-- <div class="info-balance">
                 <span class="info-value">{{
                   totalRewardsEarned | localAmountFilter
                 }}</span>
                 <span class="info-usd">{{ totalRewardsUsd | formatUSD }}</span>
-              </div>
+              </div> -->
             </div>
           </div>
         </div>
@@ -679,7 +682,7 @@ export default {
     await this.createStakePool();
 
     if (this.chainId !== 1) return false;
-    await this.getTotalRewards();
+    // await this.getTotalRewards();
     this.updateInterval = setInterval(async () => {
       await this.createStakePool();
     }, 15000);
