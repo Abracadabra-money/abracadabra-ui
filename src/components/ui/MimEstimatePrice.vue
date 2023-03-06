fc827a82d61b403711753176f33b2e16eba06a9a
0xhardrefresh <0xhardrefresh@protonmail.com>
2023-03-06 19:46:17 +0200

    update tooltip text

--- src/components/ui/MimEstimatePrice.vue
+++ src/components/ui/MimEstimatePrice.vue
@@ -135,7 +135,7 @@ export default {
     },
 
     tooltipText() {
-      return `Dynamic Fees may vary depending on current $MIM liquidity and it is not collected by the protocol. The closer $MIM is trading at peg, the lower the fee.`;
+      return `Dynamic ${this.statusText} may vary depending on current $MIM liquidity and will result in a profit when opening/closing a position.`;
     },
     buyToken() {
       if (this.itsClose) return usdt[this.chainId].address;
