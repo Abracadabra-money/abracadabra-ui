3f50a988fb14d13b3efa55e59d55d23c0be99b52
0xhardrefresh <0xhardrefresh@protonmail.com>
2023-03-06 13:14:40 +0200

    glp swapper upd

--- src/helpers/glpTEST/getGlpSwapData.js
+++ src/helpers/glpTEST/getGlpSwapData.js
@@ -1,11 +1,12 @@
 import { ethers } from "ethers";
 import { swap0xRequest } from "@/helpers/0x";
+import { actions } from "@/helpers/cauldron/cook/actions";
 
 import gmxVaultAbi from "@/helpers/glpTEST/abi/gmxVault";
 import gmxLensAbi from "@/helpers/glpTEST/abi/gmxLens";
+// import { priceAbi } from "@/utils/farmPools/abi/priceAbi";
 const gmxVaultAddress = "0x489ee077994B6658eAfA855C308275EAd8097C4A";
-const gmxLensAddress = "0xe121904194eB69e5b589b58EDCbc5B74069787C3";
-
+const gmxLensAddress = "0x9f8fB63ef774A496cd9aD3Cc29c1e81Ae947FC2e";
 import store from "@/store";
 
 const blacklis = [
@@ -13,22 +14,40 @@ const blacklis = [
   "0xfea7a6a0b346362bf88a9e4a88416b77a57d6c2a",
 ];
 
+// const getMimOraclePrice = async () => {
+//   try {
+//     const defaultProvider = new providers.StaticJsonRpcProvider(
+//       "https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"
+//     );
+
+//     const oracle = new ethers.Contract(
+//       "0x7A364e8770418566e3eb2001A96116E6138Eb32F",
+//       JSON.stringify(priceAbi),
+//       defaultProvider
+//     );
+
+//     return await oracle.latestAnswer();
+//   } catch (error) {
+//     console.log("getMimOraclePrice err:", error);
+//   }
+// };
+
 const getNameFromAddress = (address) => {
   return {
-    '0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f': 'WBTC',
-    '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1': 'WETH',
-    '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8': 'USDC',
-    '0xf97f4df75117a78c1A5a0DBb814Af92458539FB4': 'LINK',
-    '0xFa7F8980b0f1E64A2062791cc3b0871572f1F7f0': 'UNI',
-    '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9': 'USDT',
-    '0xFEa7a6a0B346362BF88A9e4A88416B77a57D6c2A': 'MIM',
-    '0x17FC002b466eEc40DaE837Fc4bE5c67993ddBd6F': 'FRAX',
-    '0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1': 'DAI',
+    "0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f": "WBTC",
+    "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1": "WETH",
+    "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8": "USDC",
+    "0xf97f4df75117a78c1A5a0DBb814Af92458539FB4": "LINK",
+    "0xFa7F8980b0f1E64A2062791cc3b0871572f1F7f0": "UNI",
+    "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9": "USDT",
+    "0xFEa7a6a0B346362BF88A9e4A88416B77a57D6c2A": "MIM",
+    "0x17FC002b466eEc40DaE837Fc4bE5c67993ddBd6F": "FRAX",
+    "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1": "DAI",
   }[address];
-}
+};
 
 const maxGlpAmount = (a, b) => {
-  return +a.glpOutAmount > +b.glpOutAmount ? a : b;
+  return +b.glpOutAmount - +a.glpOutAmount;
 };
 
 const maxBuyAmount = (a, b) => {
@@ -36,6 +55,8 @@ const maxBuyAmount = (a, b) => {
 };
 
 const getTokens = async (provider) => {
+  // const mimOraclePrice = await getMimOraclePrice();
+
   const gmxVaultContract = await new ethers.Contract(
     gmxVaultAddress,
     JSON.stringify(gmxVaultAbi),
@@ -49,13 +70,20 @@ const getTokens = async (provider) => {
   for (let i = 0; i < whitelistedTokensLength; i++) {
     const address = await gmxVaultContract.allWhitelistedTokens(i);
     // Exclude FRAX and MIM.
-    if (blacklis.indexOf(address.toLowerCase()) === -1) tokens.push(address);
+    if (blacklis.indexOf(address.toLowerCase()) === -1) {
+      // const maxAmountIn = await lensContract.getMaxAmountIn(address);
+      // const maxAmountInToMIM = maxAmountIn.div(mimOraclePrice).mul(1e8);
+      tokens.push({
+        address: address,
+        maxAmountInToMIM: ethers.BigNumber.from("1000000000000000000000"),
+      });
+    }
   }
 
   return tokens;
 };
 
-const getGlpLevData = async (provider, pool, sellAmount, chainId, slipage) => {
+const getGlpLevData = async (cookData, provider, pool, sellAmount, chainId, slipage) => {
   store.commit("updateRouteData", []);
   store.commit("setPopupState", {
     type: "test",
@@ -71,31 +99,31 @@ const getGlpLevData = async (provider, pool, sellAmount, chainId, slipage) => {
     provider
   );
 
-  for (let token of await getTokens(provider)) {
+  const tokensArr = await getTokens(provider, gmxLensContract);
+
+  for (let token of tokensArr) {
     const { buyAmount, data } = await swap0xRequest(
       chainId,
-      token,
+      token.address,
       borrowToken.address,
       slipage,
-      sellAmount,
+      sellAmount
     );
 
     const resp = await gmxLensContract.getMintedGlpFromTokenIn(
-      token,
+      token.address,
       buyAmount
     );
 
-    console.log("outAmount", resp[0].toString())
-    console.log("feesPoints", resp[1].toString())
-
     const getSwapDataEncode = ethers.utils.defaultAbiCoder.encode(
       ["bytes", "address"],
-      [data, token]
+      [data, token.address]
     );
 
     results.push({
-      tokenName: getNameFromAddress(token),
-      token,
+      ...token,
+      tokenName: getNameFromAddress(token.address),
+      token: token.address,
       glpOutAmount: resp[0].toString(),
       feesPoints: resp[1].toString(),
       swapDataEncode: getSwapDataEncode,
@@ -103,15 +131,109 @@ const getGlpLevData = async (provider, pool, sellAmount, chainId, slipage) => {
 
     store.commit("updateRouteData", results);
   }
-  const result = results.reduce(maxGlpAmount);
 
-  const notification = {
-    msg: `Token: ${getNameFromAddress(result.token)}; feesPoints: ${result.feesPoints}`,
-    type: "warning",
-  };
-  await store.dispatch("notifications/new", notification);
+  const tokenInfoArray = results.sort(maxGlpAmount);
 
-  return result;
+  let globalLimit = ethers.BigNumber.from(0);
+  tokenInfoArray.forEach((item) => {
+    globalLimit = globalLimit.add(item.maxAmountInToMIM);
+  });
+  console.log("globalLimit", globalLimit.toString());
+
+  if (sellAmount.gt(globalLimit)) console.log("to much mim to swap"); // TODO: add notification
+
+  let mimLeftToSwap = sellAmount;
+  const cookInfo = [];
+
+  for (let info of tokenInfoArray) {
+    if (mimLeftToSwap.eq(0)) break;
+    console.log("info", info);
+    console.log("mimLeftToSwap", mimLeftToSwap.toString());
+    console.log("maxAmountInToMIM", info.maxAmountInToMIM.toString());
+    const itsAmountEnough =
+      info.maxAmountInToMIM.gt(mimLeftToSwap) ||
+      info.maxAmountInToMIM.eq(mimLeftToSwap);
+    const awailableToSwap = itsAmountEnough
+      ? mimLeftToSwap
+      : info.maxAmountInToMIM;
+
+      1000000000000000000000
+      246470378101902042544
+
+    // add cook call info block
+    if (itsAmountEnough && cookInfo.length === 0) {
+      info.sellAmount = awailableToSwap;
+      cookInfo.push(info);
+    } else {
+      const { data } = await swap0xRequest(
+        chainId,
+        info.address,
+        borrowToken.address,
+        slipage,
+        awailableToSwap
+      );
+
+      const getSwapDataEncode = ethers.utils.defaultAbiCoder.encode(
+        ["bytes", "address"],
+        [data, info.address]
+      );
+
+      info.sellAmount = awailableToSwap;
+      info.swapDataEncode = getSwapDataEncode;
+
+      cookInfo.push(info);
+    }
+    //
+
+    mimLeftToSwap = mimLeftToSwap.sub(awailableToSwap);
+  }
+
+  console.log("cookInfo", cookInfo);
+  console.log("result", tokenInfoArray);
+
+  for (let info of cookInfo) {
+    const oracleExchangeRate = pool.oracleExchangeRate;
+
+    const slippageBN = ethers.BigNumber.from(
+      parseFloat(slipage * 1e10).toFixed(0)
+    );
+
+    const userAddr = store.getters.getAccount;
+    const swapAmount = info.sellAmount;
+
+    const expectedCollateral = swapAmount
+      .mul(oracleExchangeRate)
+      .div(String(1e18));
+
+    const slippageAmount = expectedCollateral
+      .div(100)
+      .mul(slippageBN)
+      .div(1e10);
+    const minExpected = expectedCollateral.sub(slippageAmount);
+
+    const swapData = info.swapDataEncode;
+    const swapperAddres = pool.levSwapperContract.address;
+
+    const swapStaticTx = await pool.levSwapperContract.populateTransaction.swap(
+      userAddr,
+      minExpected,
+      swapAmount,
+      swapData
+    );
+
+    cookData = await actions.call(
+      cookData,
+      swapperAddres,
+      swapStaticTx.data,
+      false,
+      false,
+      2
+    );
+  }
+
+  console.log("cookData", cookData);
+
+  return cookData;
 };
 
 const getGlpLiqData = async (provider, pool, amount, chainId, slipage) => {
@@ -135,7 +257,7 @@ const getGlpLiqData = async (provider, pool, amount, chainId, slipage) => {
     false
   );
   const glpAmount = await collateralToken.contract.convertToAssets(mGlpAmount);
-  console.log("glpAmount", glpAmount.toString())
+  console.log("glpAmount", glpAmount.toString());
   for (let token of await getTokens(provider)) {
     const resp = await gmxLensContract.getTokenOutFromBurningGlp(
       token,
@@ -147,7 +269,7 @@ const getGlpLiqData = async (provider, pool, amount, chainId, slipage) => {
       borrowToken.address,
       token,
       slipage,
-      resp[0].toString(),
+      resp[0].toString()
     );
 
     // console.log("outAmount", resp[0].toString())
@@ -166,7 +288,7 @@ const getGlpLiqData = async (provider, pool, amount, chainId, slipage) => {
       buyAmount,
       feesPoints: resp[1].toString(),
       swapDataEncode: getSwapDataEncode,
-      sellAmount
+      sellAmount,
     });
 
     store.commit("updateRouteData", results);
@@ -179,8 +301,8 @@ const getGlpLiqData = async (provider, pool, amount, chainId, slipage) => {
     type: "warning",
   };
   await store.dispatch("notifications/new", notification);
-  console.log("tokenOutAmount", result.tokenOutAmount)
-  console.log("sellAmount", result.sellAmount.toString())
+  console.log("tokenOutAmount", result.tokenOutAmount);
+  console.log("sellAmount", result.sellAmount.toString());
   return result;
 };
 
