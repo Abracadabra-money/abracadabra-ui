13062d0995ddca6317545b8fe74ca66ce4ed6c6d
Andrey <onipchenko.andrey.90@gmail.com>
2023-03-31 16:09:59 +0300

    added new tooltip VueTippy

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ethers } from "ethers";
 import { createApp } from "vue";
 import connectWallet from "./plugins/connectWallet";
 import clickOutside from "./directives/clickOutside";
+import VueTippy from "vue-tippy";
 
 const app = createApp(App);
 
@@ -12,11 +13,18 @@ app.use(store);
 app.use(router);
 app.use(connectWallet);
 app.directive("click-outside", clickOutside);
+app.use(VueTippy, {
+  directive: "tooltip",
+  defaultProps: {
+    placement: "auto",
+    allowHTML: true,
+  },
+});
 app.mount("#app");
 
-const useImage = ((url: any) => {
-    return new URL(`/src/${url}`, import.meta.url).href;
-});
+const useImage = (url: any) => {
+  return new URL(`/src/${url}`, import.meta.url).href;
+};
 
 app.config.globalProperties.$ethers = ethers;
 app.config.globalProperties.$image = useImage;
