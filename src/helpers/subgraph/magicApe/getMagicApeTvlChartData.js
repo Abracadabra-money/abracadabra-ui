4fed0309c77b1b1c9d27dde4d21d39b4f52dd8b6
Андрей <47685068+Andrey-Onipchenko@users.noreply.github.com>
2023-04-18 18:35:56 +0300

    Vue3 update chart and total rewards magic ape (#105)
    * update Yield chart data magicApe
    
    * updated magicApe chart Yield, TVL, Price and total Rewards
    
    * deleted test button
    
    ---------
    
    Co-authored-by: Andrey <onipchenko.andrey.90@gmail.com>

---
+++ src/helpers/subgraph/magicApe/getMagicApeTvlChartData.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import moment from "moment";
+import { markRaw } from "vue";
+import { getGraphUrl } from "@/helpers/subgraph/utils";
+
+const getMagicApeTvlChartData = async (month = 1, chainId = 1) => {
+  const secondsPerDay = 60 * 60 * 24;
+  const days = 30 * month;
+  const to = Math.floor(Date.now() / 1000 / secondsPerDay);
+  const from = to - days;
+  const url = getGraphUrl(chainId);
+
+  const query = `{
+        magicApeTvlDailySnapshots(
+            first: ${days}
+            where: {id_gte: ${from}, id_lte: ${to}}
+            orderBy: id
+            orderDirection: desc
+        ) {
+            id
+            totalValueLockedUsd
+        }
+    }`;
+  const { data } = await axios.default.post(url, { query });
+  const snapshots = data.data?.magicApeTvlDailySnapshots;
+
+  const result = [];
+
+  snapshots.forEach((snapshot) => {
+    result.push({
+      tvl: snapshot.totalValueLockedUsd,
+      date: moment.unix(snapshot.id * secondsPerDay).format("YYYY-MM-DD"),
+    });
+  });
+
+  return markRaw(result);
+};
+
+export { getMagicApeTvlChartData };
