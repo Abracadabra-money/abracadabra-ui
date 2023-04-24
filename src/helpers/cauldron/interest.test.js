7d716232711c81ee63e748972891368a35c60da5
0xhardrefresh <0xhardrefresh@protonmail.com>
2023-04-24 21:58:21 +0300

    update interest test

--- src/helpers/cauldron/interest.test.js
+++ src/helpers/cauldron/interest.test.js
@@ -28,6 +28,18 @@ describe("getInterest()", () => {
     expect(Number(result)).toBeGreaterThan(0);
   });
 
+  it("should return 0 if accrueInfo INTEREST_PER_SECOND is 0", async () => {
+    const dummyCauldron = {
+      accrueInfo: () => {
+        return {
+          INTEREST_PER_SECOND: BigNumber.from(0),
+        };
+      },
+    };
+    const result = await getInterest(dummyCauldron);
+    expect(Number(result)).toBe(0);
+  });
+
   it("shoud return 0 if accrueInfo has no INTEREST_PER_SECOND", async () => {
     const dummyCauldron = {
       accrueInfo: () => {
