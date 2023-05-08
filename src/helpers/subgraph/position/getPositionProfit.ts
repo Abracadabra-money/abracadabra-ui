abf7d19807340f290f418cad4a254cd604406ca7
Andrey <onipchenko.andrey.90@gmail.com>
2023-05-08 17:04:42 +0300

    added positions profit

--- src/helpers/subgraph/position/getPositionProfit.ts
+++ src/helpers/subgraph/position/getPositionProfit.ts
@@ -7,48 +7,22 @@ export const getPositionProfit = async (address: string) => {
         account(id: "${address.toLowerCase()}") {
           id
           states {
-            id
             borrowPart
             collateralShare
             cauldron {
-              name
               id
               collateral {
                 lastPriceUsd
                 name
-                symbol
                 decimals
               }
             }
             snapshots(orderBy: timestamp, orderDirection: asc) {
               withdrawAmountUsd
-              withdrawAmount
-              timestamp
-              repaid
               repaidUsd
-              liquidationPrice
-              isLiquidated
-              id
-              hash
               collateralShare
               collateralPriceUsd
               borrowPart
-              blockNumber
-            }
-            lastAction {
-              withdrawAmountUsd
-              withdrawAmount
-              timestamp
-              repaid
-              repaidUsd
-              liquidationPrice
-              isLiquidated
-              id
-              hash
-              collateralShare
-              collateralPriceUsd
-              borrowPart
-              blockNumber
             }
           }
         }
@@ -56,59 +30,40 @@ export const getPositionProfit = async (address: string) => {
 
   const { data } = await axios.post(url, { query });
 
-  const result = data.data.account.states.map(
+  return data.data.account.states.map(
     ({ borrowPart, collateralShare, cauldron, snapshots }: any) => {
       if (+borrowPart === 0 && +collateralShare === 0) return 0;
-
       const { decimals, lastPriceUsd } = cauldron.collateral;
-      if (+borrowPart === 0) {
-        const parseColateral = +utils.formatUnits(collateralShare, decimals);
 
-        const firstPrice = parseColateral * +snapshots[0].collateralPriceUsd;
+      const startCollateralUsd =
+        +utils.formatUnits(snapshots[0].collateralShare, decimals) *
+        +snapshots[0].collateralPriceUsd;
 
-        const lastPrice = parseColateral * +lastPriceUsd;
+      const startBorrowUsd = +utils.formatUnits(snapshots[0].borrowPart, 18);
 
-        return { profit: lastPrice - firstPrice, cauldron: cauldron.id };
+      const curentCollateralUsd =
+        +utils.formatUnits(collateralShare, decimals) * lastPriceUsd;
+
+      const curentBorrowUsd = +utils.formatUnits(borrowPart, 18);
+
+      let withdrawUsd = 0;
+      let repaidUsd = 0;
+
+      for (let i = 1; i < snapshots.length; i++) {
+        withdrawUsd = withdrawUsd + +snapshots[i].withdrawAmountUsd;
+        repaidUsd = repaidUsd + +snapshots[i].repaidUsd;
       }
+
+      const profit =
+        curentCollateralUsd +
+        withdrawUsd -
+        (curentBorrowUsd + repaidUsd) -
+        (startCollateralUsd - startBorrowUsd);
+
+      return {
+        ...cauldron,
+        profit,
+      };
     }
   );
-
-  console.log(result);
-
-  // const result = {
-  //   withdrawAmountUsd: 0,
-  //   withdrawAmount: ethers.utils.parseUnits("0", collateral.decimals),
-  // };
-
-  // console.log("result 111", result);
-
-  // snapshots.forEach((element) => {
-  //   result.withdrawAmountUsd =
-  //     result.withdrawAmountUsd + +element.withdrawAmountUsd;
-
-  //   // console.log(
-  //   //   "result.withdrawAmount",
-  //   //   ethers.utils.formatEther(result.withdrawAmount)
-  //   // );
-  //   // console.log(
-  //   //   "element.withdrawAmount,",
-  //   //   ethers.utils.formatEther(element.withdrawAmount)
-  //   // );
-
-  //   result.withdrawAmount = result.withdrawAmount
-  //     .add(ethers.utils.parseUnits(element.withdrawAmount, collateral.decimals))
-  //     .div(Math.pow(10, 18).toString());
-  // });
-
-  // // const test = ethers.utils
-  // //   .parseEther("0")
-  // //   .add(ethers.utils.parseUnits("4043554092343899278602", 18))
-  // //   .div(Math.pow(10, 18).toString());
-
-  // // console.log("result", collateral.decimals);
-  // console.log("result", ethers.utils.formatEther(result.withdrawAmount));
-
-  // const firstAction = snapshots[0];
-
-  return data;
 };
