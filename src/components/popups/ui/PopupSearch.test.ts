295a7cba269dc2d9c934a2e1dbdcd1562fd50aaa
Andrey <onipchenko.andrey.90@gmail.com>
2023-05-26 11:48:31 +0300

    added test for component PopupSearch

---
+++ src/components/popups/ui/PopupSearch.test.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from "vitest";
+import { mount } from "@vue/test-utils";
+import PopupSearch from "@/components/popups/ui/PopupSearch.vue";
+
+describe("PopupEmptyState.vue", () => {
+  it("should display the search input", () => {
+    const wrapper = mount(PopupSearch);
+    const input = wrapper.find(".search-input");
+    expect(input.attributes().placeholder).toContain("Search");
+  });
+});
