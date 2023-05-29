3c254fdd4ea14e075bf976d8ba90688e25b2e0ae
5450L <susalafamilia@gmail.com>
2023-05-29 12:51:45 +0300

    icons import fixed

--- src/components/myPositions/PositionAssets.test.ts
+++ src/components/myPositions/PositionAssets.test.ts
@@ -2,23 +2,20 @@ import { mount } from "@vue/test-utils";
 import { describe, it, expect } from "vitest";
 import PositionAssets from "@/components/myPositions/PositionAssets.vue";
 
-import { ethers } from "ethers";
 import filters from "@/filters/index.js";
-import mimIcon from "@/assets/images/tokens/MIM.png";
-import spellIcon from "@/assets/images/tokens/SPELL.png";
 
 const assetsInfoTest = [
   {
     title: "Collateral Deposited",
     symbol: "SPELL",
-    icon: spellIcon,
+    icon: "@/assets/images/tokens/SPELL.png",
     amount: filters.formatTokenBalance(1000000000000000000),
     amountUsd: filters.formatUSD(1000000000000000),
   },
   {
     title: "Borrowed",
     symbol: "MIM",
-    icon: mimIcon,
+    icon: "@/assets/images/tokens/MIM.png",
     amount: filters.formatTokenBalance(10000000000000000000),
   },
 ];
@@ -36,7 +33,7 @@ describe("PositionAssets.vue", () => {
       props: { assetsInfo: assetsInfoTest },
     });
 
-    const assetsItems = wrapper.findAll(".assets-item").at(0);
+    const assetsItems = wrapper.findAll(".assets-item").at(0)!;
     expect(assetsItems.classes()).toContain("assets-item");
   });
 });
