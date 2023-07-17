254dba52e2253d1747a23abef1234b18ba27c9e7
Andrey <onipchenko.andrey.90@gmail.com>
2023-07-17 14:36:11 +0300

    Updated coingeckoChainId

--- src/utils/beam/scanConfig.ts
+++ src/utils/beam/scanConfig.ts
@@ -5,6 +5,7 @@ const config = {
   137: { name: "Polygonscan", url: "https://polygonscan.com/tx/" },
   250: { name: "Ftmscan", url: "https://ftmscan.com/tx/" },
   1285: { name: "Moonscan", url: "https://moonriver.moonscan.io/tx/" },
+  2222: { name: "Explorer Kava", url: "https://explorer.kava.io/tx/" },
   42161: { name: "Arbiscan", url: "https://arbiscan.io/tx/" },
   43114: { name: "Snowtrace", url: "https://snowtrace.io/tx/" },
 };
