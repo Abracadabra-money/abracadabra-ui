b5bfa7840763b12f3fd0e53c54d0afe3c164fc82
5450L <susalafamilia@gmail.com>
2023-07-20 18:06:28 +0300

    Added WalletButton.test.ts

--- src/components/ui/buttons/WalletButton.test.ts
+++ src/components/ui/buttons/WalletButton.test.ts
@@ -1,14 +1,14 @@
 import { describe, it, expect } from "vitest";
 import { mount } from "@vue/test-utils";
-import SettingsButton from "@/components/ui/buttons/SettingsButton.vue";
+import WalletButton from "@/components/ui/buttons/WalletButton.vue";
 
-describe("SettingsButton.vue", () => {
-  const wrapper: any = mount(SettingsButton);
+describe("WalletButton.vue", () => {
+  const wrapper: any = mount(WalletButton);
 
   it("Should render without props with no 'active' class", async () => {
-    const settingsBtn = wrapper.find(".settings-btn");
-    expect(settingsBtn.exists()).toBe(true);
-    expect(settingsBtn.classes("active")).toBe(false);
+    const walletBtn = wrapper.find(".wallet-btn");
+    expect(walletBtn.exists()).toBe(true);
+    expect(walletBtn.classes("active")).toBe(false);
   });
 
   it("Should render with props and set 'active' class depends on it", async () => {
