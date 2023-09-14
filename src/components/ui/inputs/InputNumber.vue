ec5b850516fd91e0621332ff8a39e26391edc7f6
Andrey <onipchenko.andrey.90@gmail.com>
2023-09-14 17:22:51 +0300

    updated InputNumber component

--- src/components/ui/inputs/InputNumber.vue
+++ src/components/ui/inputs/InputNumber.vue
@@ -19,6 +19,26 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+input[type="number"]::-webkit-outer-spin-button,
+input[type="number"]::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+input[type="number"] {
+  -moz-appearance: textfield;
+}
+input[type="number"]:hover,
+input[type="number"]:focus {
+  -moz-appearance: number-input;
+}
+
+input[type="number"]::-webkit-inner-spin-button,
+input[type="number"]::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
 .input-number {
   height: 50px;
   text-align: center;
