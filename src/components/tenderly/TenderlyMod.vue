1676053ff4496a9ec7dafc132a4e9881018b7501
Andrey <onipchenko.andrey.90@gmail.com>
2023-09-19 16:51:22 +0300

    updated TenderlyMod component

--- src/components/tenderly/TenderlyMod.vue
+++ src/components/tenderly/TenderlyMod.vue
@@ -1,87 +1,110 @@
 <template>
-  <div class="test">
-    <div
-      class="wrap"
-      v-if="isTenderlyMod"
-      @click="test"
-      @mousemove="upHere = true"
+  <div class="tenderly-mod" v-if="!!activeForkData">
+    <button
+      class="tenderly-tab-button"
+      @click="goToTenderlyTap"
+      @mousemove="isAdditionalButtons = true"
     >
       <img
-        class="icon"
+        class="tenderly-tab-icon"
         src="@/assets/images/tenderly/tenderly_icon.png"
-        alt=""
+        alt="Tenderly Tab icon"
       />
-    </div>
+    </button>
 
     <img
-      v-tooltip="'Delete Fork'"
-      class="dis-icon"
-      v-show="upHere"
+      class="switch-mod"
+      v-show="isAdditionalButtons"
+      v-tooltip="'Toggle use fork'"
+      @click="toggleTenderlyMod"
       src="@/assets/images/tenderly/disconnect_fork.png"
-      alt=""
-      @click="disconnectDevMod"
+      alt="Toggle use fork"
     />
 
     <img
+      class="add-to-metamask"
+      v-show="isAdditionalButtons"
       v-tooltip="'Add fork to metamask'"
-      class="add"
-      v-show="upHere"
+      @click="addAndSwitch"
       src="@/assets/images/tenderly/add_fork.png"
-      alt=""
-      @click="addForkToMetamaskAction"
+      alt="Add fork to metamask"
     />
   </div>
 </template>
 
 <script>
-import { mapActions } from "vuex";
-import { addForkToMetamask } from "@/helpers/tenderly/addForkToMetamask";
+import {
+  TENDERLY_FORK_DATA,
+  TENDERLY_EVENT_CHANGED_DATA,
+} from "@/constants/tenderly";
+import { mapActions, mapGetters } from "vuex";
+import { networksConfig } from "@/utils/networks/networksConfig";
+import notification from "@/helpers/notification/notification.js";
+import { addAndSwitchForkOnWallet } from "@/helpers/tenderly/addAndSwitchForkOnWallet";
 
 export default {
   data() {
     return {
-      upHere: false,
-      isTenderlyMod: !!localStorage.getItem("tenderly_fork_data"),
+      isAdditionalButtons: false,
+      forksData: JSON.parse(localStorage.getItem("tenderly_fork_data")),
     };
+  },
+
+  computed: {
+    ...mapGetters({
+      chainId: "getChainId",
+    }),
+
+    activeForkData() {
+      return this.forksData.find((data) => {
+        if (data?.useFork) return data;
+      });
+    },
   },
 
   methods: {
     ...mapActions({ createNotification: "notifications/new" }),
-    test() {
+
+    goToTenderlyTap() {
       this.$router.push({ name: "TenderlyTap" });
     },
 
-    disconnectDevMod() {
-      localStorage.removeItem("tenderly_fork_data");
+    async toggleTenderlyMod() {
+      this.forksData.find((fork) => {
+        fork.useFork = false;
+      });
+
+      localStorage.setItem(TENDERLY_FORK_DATA, JSON.stringify(this.forksData));
       window.location.reload();
     },
 
-    async addForkToMetamaskAction() {
-      const forkData = JSON.parse(localStorage.getItem("tenderly_fork_data"));
+    async addAndSwitch() {
+      const networkConfig = networksConfig.find(
+        (network) => network.chainId === this.chainId
+      );
 
-      await addForkToMetamask(forkData);
-      window.location.reload();
+      const { error } = await addAndSwitchForkOnWallet(
+        this.activeForkData,
+        networkConfig.switchData
+      );
+      if (!error) window.location.reload();
     },
   },
 
   async mounted() {
-    window.addEventListener("tenderly_fork_data-changed", () => {
-      this.isTenderlyMod = !!localStorage.getItem("tenderly_fork_data");
+    window.addEventListener(TENDERLY_EVENT_CHANGED_DATA, () => {
+      this.forksData = JSON.parse(localStorage.getItem(TENDERLY_FORK_DATA));
     });
 
-    // todo Notification
-    if (this.isTenderlyMod) {
-      await this.createNotification({
-        msg: "You have Tenderly mod enabled. Make sure that Fork is selected in your wallet. Or log out.",
-        type: "warning",
-      });
+    if (this.activeForkData) {
+      await this.createNotification(notification.tenderlyMod);
     }
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.test {
+.tenderly-mod {
   width: 50px;
   height: 50px;
   border-radius: 50%;
@@ -89,22 +112,22 @@ export default {
   bottom: 40px;
   right: 40px;
   transition: all 0.5s ease-in-out;
+}
 
-  &:hover {
-    width: 130px;
-    height: 130px;
-    bottom: 0px;
-    right: 0px;
+.tenderly-mod:hover {
+  width: 130px;
+  height: 130px;
+  bottom: 0px;
+  right: 0px;
 
-    .dis-icon,
-    .add {
-      visibility: visible;
-      opacity: 1;
-    }
+  .switch-mod,
+  .add-to-metamask {
+    visibility: visible;
+    opacity: 1;
   }
 }
 
-.wrap {
+.tenderly-tab-button {
   position: fixed;
   bottom: 40px;
   right: 40px;
@@ -116,13 +139,17 @@ export default {
   justify-content: center;
   cursor: pointer;
   align-items: center;
+  border: none;
+  outline: transparent;
 }
 
-.icon {
+.tenderly-tab-icon {
   max-width: 30px;
 }
 
-.dis-icon {
+.switch-mod,
+.add-to-metamask {
+  cursor: pointer;
   visibility: hidden;
   position: fixed;
   opacity: 0;
@@ -131,17 +158,9 @@ export default {
   width: 40px;
   height: 40px;
   transition: all 0.5s ease-in-out;
-  cursor: pointer;
 }
-.add {
-  visibility: hidden;
-  cursor: pointer;
-  position: fixed;
-  opacity: 0;
+.add-to-metamask {
   bottom: 45px;
   right: 95px;
-  width: 40px;
-  height: 40px;
-  transition: all 0.5s ease-in-out;
 }
 </style>
