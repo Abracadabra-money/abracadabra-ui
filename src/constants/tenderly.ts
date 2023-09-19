5eeca8442b7d71fdd57a8df889373c60391a3cb0
Andrey <onipchenko.andrey.90@gmail.com>
2023-09-19 14:31:41 +0300

    added deletFork helper and updated constans

--- src/constants/tenderly.ts
+++ src/constants/tenderly.ts
@@ -1,7 +1,7 @@
-export const TENDERLY_URL: string = `https://api.tenderly.co/api/v1/account/${
+export const TENDERLY_BASE_URL: string = `https://api.tenderly.co/api/v1/account/${
   import.meta.env.VITE_APP_TENDERLY_USER
-}/project/${import.meta.env.VITE_APP_TENDERLY_PROJECT}/fork`;
+}/project/${import.meta.env.VITE_APP_TENDERLY_PROJECT}`;
 
 export const TENDERLY_FORK_URL: string = "https://rpc.tenderly.co/fork/";
-export const TENDERLY_CUSTOM_EVENT: string = "tenderly_fork_data-changed";
+export const TENDERLY_EVENT_CHANGED_DATA: string = "tenderly_fork_data-changed";
 export const TENDERLY_FORK_DATA: string = "tenderly_fork_data";
--- src/helpers/tenderly/tenderlyDispatchEvent.ts
+++ src/helpers/tenderly/tenderlyDispatchEvent.ts
@@ -1,11 +1,11 @@
 import {
-  TENDERLY_CUSTOM_EVENT,
+  TENDERLY_EVENT_CHANGED_DATA,
   TENDERLY_FORK_DATA,
 } from "@/constants/tenderly";
 
 export const tenderlyDispatchEvent = () => {
   window.dispatchEvent(
-    new CustomEvent(TENDERLY_CUSTOM_EVENT, {
+    new CustomEvent(TENDERLY_EVENT_CHANGED_DATA, {
       detail: {
         storage: localStorage.getItem(TENDERLY_FORK_DATA),
       },
