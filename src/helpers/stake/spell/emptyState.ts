26220edbb7832dad95e98470eb37d06084910b78
Andrey <onipchenko.andrey.90@gmail.com>
2023-09-27 09:03:08 +0300

    updated spell empty state

--- src/helpers/stake/spell/emptyState.ts
+++ src/helpers/stake/spell/emptyState.ts
@@ -7,15 +7,6 @@ import { getSpellToSSpellRate } from "@/helpers/stake/spell/getSpellToSSpellRate
 const config = spellConfig[1 as keyof typeof spellConfig];
 const { spell, sSpell, mSpell }: any = config;
 
-export const sSpellEmptyState: EmptyTokenState = {
-  icon: sSpell?.icon || useImage("assets/images/sspell-icon.svg"),
-  name: sSpell?.name || "sSpell",
-  balance: 0n,
-  rate: await getSpellToSSpellRate(spell, sSpell.contract),
-  price: ONE_ETHER_VIEM,
-  decimals: 18,
-};
-
 const spellEmptyState: EmptyTokenState = {
   icon: spell.icon,
   name: spell.name,
@@ -34,8 +25,25 @@ const mSpellEmptyState: EmptyTokenState = {
   decimals: 18,
 };
 
-export const emptyState: EmptySpellState = {
-  spell: spellEmptyState,
-  sSpell: sSpellEmptyState,
-  mSpell: mSpellEmptyState,
+export const getSSpellEmptyState = async (): Promise<EmptyTokenState> => {
+  const spellToSSpellRate = await getSpellToSSpellRate(spell, sSpell.contract);
+
+  return {
+    icon: sSpell?.icon || useImage("assets/images/sspell-icon.svg"),
+    name: sSpell?.name || "sSpell",
+    balance: 0n,
+    rate: spellToSSpellRate,
+    price: ONE_ETHER_VIEM,
+    decimals: 18,
+  };
+};
+
+export const getSpellEmptyState = async (): Promise<EmptySpellState> => {
+  const sSpell = await getSSpellEmptyState();
+
+  return {
+    sSpell,
+    spell: spellEmptyState,
+    mSpell: mSpellEmptyState,
+  };
 };
