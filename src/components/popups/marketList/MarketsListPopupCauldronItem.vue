b6b05bbb84e1d9759bfc801559fd317a062149fe
5450L <susalafamilia@gmail.com>
2023-09-29 15:57:29 +0300

    popup item template fix

--- src/components/popups/marketList/MarketsListPopupCauldronItem.vue
+++ src/components/popups/marketList/MarketsListPopupCauldronItem.vue
@@ -5,24 +5,28 @@
       <div class="cauldron-info">
         <BaseTokenIcon :icon="configItem.icon" :name="configItem.name" />
         <div>
-          <span>{{ configItem.name }}</span>
-          <span class="tooltip" v-tooltip="'Interest'"
-            >{{ marketItem.interest }}%</span
-          >
+          <div class="value-title">
+            <span class="cauldron name">{{ configItem.name }}</span>
+            <span class="tooltip" v-tooltip="'Interest'"
+              >{{ marketItem.interest }}%</span
+            >
+          </div>
           <MiniStatusTag v-if="isMigrated" />
           <MiniStatusTag v-if="isUnrappedToken" text="Leverage" />
         </div>
       </div>
 
       <div class="cauldron-balance" v-if="isUnrappedToken">
-        <p>
+        <div class="value-title">
           {{ formatTokenBalance(userInfo.collateralAmount) }}
-          {{ configItem.name }}
-        </p>
-        <p>
+          <span class="token name">{{ configItem.name }}</span>
+        </div>
+        <div class="value-title">
           {{ formatTokenBalance(userInfo.unwrappedTokenAmount) }}
-          {{ configItem.wrapInfo.unwrappedToken.name }}
-        </p>
+          <span class="token name">{{
+            configItem.wrapInfo.unwrappedToken.name
+          }}</span>
+        </div>
         <p class="price" v-if="userTotalBalance">
           Total amount: {{ formatUSD(userTotalBalance) }}
         </p>
@@ -143,9 +147,22 @@ export default {
   align-items: center;
 }
 
+.value-title {
+  display: flex;
+  align-items: center;
+  gap: 5px;
+}
+.name {
+  display: inline-block;
+  max-width: 120px;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+  text-align: start;
+}
+
 .tooltip {
   display: inline-block;
-  margin-left: 5px;
   font-size: 13px;
   color: rgba(255, 255, 255, 0.6);
 }
