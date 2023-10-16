4fab81e97736dc0a6989ca70befe2e6921cccd1c
5450L <susalafamilia@gmail.com>
2023-10-16 15:47:43 +0300

    checkSanctionList for new networks fixed

--- src/plugins/connectWalletV2/checkSanctionAddress.ts
+++ src/plugins/connectWalletV2/checkSanctionAddress.ts
@@ -1,12 +1,18 @@
 import store from "@/store";
-import { readContract } from "@wagmi/core";
 import { sanctionAbi } from "@/utils/abi/sanctionAbi";
 import notification from "@/helpers/notification/notification.js";
 import { SANCTIONS_LIST_ADDRESS } from "@/constants/tokensAddress";
 import type { Address } from "@wagmi/core";
+import { createPublicClient, http } from "viem";
+import { mainnet } from "viem/chains";
 
 export const checkSanctionAddress = async (address: Address | string) => {
-  const isSanctioned = await readContract({
+  const publicClient = createPublicClient({
+    chain: mainnet,
+    transport: http(),
+  });
+
+  const isSanctioned = await publicClient.readContract({
     address: SANCTIONS_LIST_ADDRESS,
     abi: sanctionAbi,
     functionName: "isSanctioned",
