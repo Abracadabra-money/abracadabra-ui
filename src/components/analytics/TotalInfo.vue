0c891dfe0f6878ad2a1bb767c1da08814dea4079
5450L <susalafamilia@gmail.com>
2023-11-03 13:08:29 +0200

    Total helpers reworked

--- src/components/analytics/TotalInfo.vue
+++ src/components/analytics/TotalInfo.vue
@@ -14,28 +14,21 @@
       <p class="card-value">$ {{ totalMimBorrowed }}</p>
     </div>
 
-    <div class="card">
-      <h4 class="cart-title">Borrowing Fees</h4>
-      <p class="card-value">$ {{ borrowingFees.formatted }}</p>
-    </div>
-    <div class="card">
-      <h4 class="cart-title">Interest Fees</h4>
-      <p class="card-value">$ {{ interestFees.formatted }}</p>
-    </div>
-    <div class="card">
-      <h4 class="cart-title">Liquidation Fees</h4>
-      <p class="card-value">$ {{ liquidationFees.formatted }}</p>
-    </div>
-    <div class="card">
-      <h4 class="cart-title">Unregistered Fees</h4>
-      <p class="card-value">$ {{ unregisteredFees.formatted }}</p>
+    <div class="card" v-for="category in feesByCategory">
+      <h4 class="cart-title">{{ category.name }}</h4>
+      <p class="card-value">$ {{ category.value }}</p>
     </div>
   </div>
 </template>
 
 <script>
-import { getTotal } from "@/helpers/analytics/getTotal";
-import { getFees } from "@/helpers/analytics/getFees";
+import {
+  getTotal,
+  getByChain,
+  getByCauldron,
+  getFeesByCategory,
+  queryMethods,
+} from "@/helpers/analytics/getTotal";
 
 import filters from "@/filters";
 
@@ -44,86 +37,39 @@ export default {
     cauldronsData: { type: Array },
   },
 
-  computed: {
-    queryMethods() {
-      return {
-        VALUE_LOCKED: "totalValueLockedUsd",
-        FEES_GENERATED: "totalFeesGenerated",
-        MIM_BORROWED: "totalMimBorrowed",
-        BORROWING_FEES: "borrowFeesGenerated",
-        INTEREST_FEES: "interestFeesGenerated",
-        LIQUIDATION_FEES: "liquidationFeesGenerated",
-      };
-    },
+  data() {
+    return {
+      feesByCategory: [],
+    };
+  },
 
+  computed: {
     totalValueLocked() {
       return filters.formatNumber(
-        getTotal(this.cauldronsData, this.queryMethods.VALUE_LOCKED)
+        getTotal(this.cauldronsData, queryMethods.VALUE_LOCKED)
       );
     },
 
     totalMimBorrowed() {
       return filters.formatNumber(
-        getTotal(this.cauldronsData, this.queryMethods.MIM_BORROWED)
+        getTotal(this.cauldronsData, queryMethods.MIM_BORROWED)
       );
     },
 
     totalFeesGenerated() {
       const totalFeesGenerated = getTotal(
         this.cauldronsData,
-        this.queryMethods.FEES_GENERATED
+        queryMethods.FEES_GENERATED
       );
       return {
         native: totalFeesGenerated,
         formatted: filters.formatNumber(totalFeesGenerated),
       };
     },
+  },
 
-    borrowingFees() {
-      const borrowingFees = getFees(
-        this.cauldronsData,
-        this.queryMethods.BORROWING_FEES
-      );
-      return {
-        native: borrowingFees,
-        formatted: filters.formatNumber(borrowingFees),
-      };
-    },
-
-    interestFees() {
-      const interestFees = getFees(
-        this.cauldronsData,
-        this.queryMethods.INTEREST_FEES
-      );
-      return {
-        native: interestFees,
-        formatted: filters.formatNumber(interestFees),
-      };
-    },
-
-    liquidationFees() {
-      const liquidationFees = getFees(
-        this.cauldronsData,
-        this.queryMethods.LIQUIDATION_FEES
-      );
-      return {
-        native: liquidationFees,
-        formatted: filters.formatNumber(liquidationFees),
-      };
-    },
-
-    unregisteredFees() {
-      const unregisteredFees =
-        this.totalFeesGenerated.native -
-        this.interestFees.native -
-        this.liquidationFees.native -
-        this.borrowingFees.native;
-
-      return {
-        native: unregisteredFees,
-        formatted: filters.formatNumber(unregisteredFees),
-      };
-    },
+  async created() {
+    this.feesByCategory = await getByChain(queryMethods.MIM_BORROWED);
   },
 };
 </script>
--- src/helpers/analytics/getTotal.ts
+++ src/helpers/analytics/getTotal.ts
@@ -1,3 +1,25 @@
+import axios from "axios";
+import { getGraphUrl } from "./getGraphUrl";
+
+type Chain = { name: string; id: number };
+
+export const queryMethods = {
+  VALUE_LOCKED: "totalValueLockedUsd",
+  FEES_GENERATED: "totalFeesGenerated",
+  MIM_BORROWED: "totalMimBorrowed",
+  BORROWING_FEES: "borrowFeesGenerated",
+  INTEREST_FEES: "interestFeesGenerated",
+  LIQUIDATION_FEES: "liquidationFeesGenerated",
+};
+
+const chains: Chain[] = [
+  { name: "Ethereum", id: 1 },
+  { name: "Optimism", id: 10 },
+  { name: "Fantom", id: 250 },
+  { name: "Arbitrum", id: 42161 },
+  { name: "Avalanche", id: 43114 },
+];
+
 export const getTotal = (cauldronsData: any, queryMethod: string) => {
   let total = 0;
 
@@ -6,4 +28,88 @@ export const getTotal = (cauldronsData: any, queryMethod: string) => {
   });
 
   return total;
+};
+
+export const getByChain = async (queryMethod: string) => {
+  const query = `{
+  protocols {
+    ${queryMethod}
+  }
+}`;
+
+  const totalByChain = await Promise.all(
+    chains.map(async (chain: Chain) => {
+      const { data } = await axios.post(getGraphUrl(chain.id), { query });
+      return {
+        value: +data.data.protocols[0][queryMethod],
+        name: chain.name,
+      };
+    })
+  );
+
+  return totalByChain;
+};
+
+export const getByCauldron = async (queryMethod: string) => {
+  const query = `{
+    cauldrons {
+      ${queryMethod}
+      name
+    }
+  }`;
+
+  const byCauldron = await Promise.all(
+    chains.map(async (chain: Chain) => {
+      const { data } = await axios.post(getGraphUrl(chain.id), { query });
+      return data.data.cauldrons
+        .filter((cauldron: any) => +cauldron[queryMethod] > 0)
+        .map((cauldron: any) => {
+          return { value: +cauldron[queryMethod], name: cauldron.name };
+        });
+    })
+  );
+
+  return byCauldron.flat();
+};
+
+export const getFeesByCategory = async () => {
+  const query = `{
+        protocols {
+          totalFeesGenerated
+          borrowFeesGenerated
+          interestFeesGenerated
+          liquidationFeesGenerated
+        }
+      }`;
+
+  let feesByCategory = [
+    { name: "borrowFees", value: 0 },
+    { name: "interestFees", value: 0 },
+    { name: "liquidationFees", value: 0 },
+    { name: "unregisteredFees", value: 0 },
+  ];
+
+  await Promise.all(
+    chains.map(async (chain: Chain) => {
+      const { data } = await axios.post(getGraphUrl(chain.id), { query });
+      const {
+        borrowFeesGenerated,
+        interestFeesGenerated,
+        liquidationFeesGenerated,
+        totalFeesGenerated,
+      } = data.data.protocols[0];
+      const unregisteredFees =
+        +totalFeesGenerated -
+        +borrowFeesGenerated -
+        +interestFeesGenerated -
+        +liquidationFeesGenerated;
+
+      feesByCategory[0].value += +borrowFeesGenerated;
+      feesByCategory[1].value += +interestFeesGenerated;
+      feesByCategory[2].value += +liquidationFeesGenerated;
+      feesByCategory[3].value += +unregisteredFees;
+    })
+  );
+
+  return feesByCategory;
 };
