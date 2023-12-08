6c3ca4769e177ff13ed8adfff398ea10412287ce
0xhardrefresh <98540076+0xhardrefresh@users.noreply.github.com>
2023-12-08 12:42:07 +0200

    👷‍♂️  Added GM apr cache logic (#262)
    * added GM apr cache logic
    
    * remove console

--- src/helpers/collateralsApy/getGMApr.js
+++ src/helpers/collateralsApy/getGMApr.js
@@ -1,13 +1,13 @@
 import { getMarketsApr } from "../gm/apr/getMarketApr";
-import { utils } from "ethers";
+import { utils, BigNumber } from "ethers";
 
 export const ABRA_FEES = 1000; // 10% 
 export const BASIS_POINTS_DIVISOR = 10000;
 
 export const getGMApr = async (market, provider) => {
   const marketsApr = await getMarketsApr(provider);
-  const aprData = marketsApr.marketsTokensAPRData[market];
-  const incentiveAprData = marketsApr.marketsTokensIncentiveAprData[market];
+  const aprData = BigNumber.from(marketsApr.marketsTokensAPRData[market]);
+  const incentiveAprData = BigNumber.from(marketsApr.marketsTokensIncentiveAprData[market]);
 
   const feesBasicPoints = BASIS_POINTS_DIVISOR - ABRA_FEES
   const bonusApr = incentiveAprData
