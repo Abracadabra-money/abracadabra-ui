f559bfaa12c3a9dbe7851442221324ae7636d74e
0xCalibur <92554750+0xCalibur@users.noreply.github.com>
2023-12-13 18:58:16 -0500

    Update index.ts

--- src/constants/gm/index.ts
+++ src/constants/gm/index.ts
@@ -2,7 +2,7 @@ import { expandDecimals } from "@/helpers/gm/fee/expandDecials";
 import type { Address } from "viem";
 
 export const ORDER_AGENT: Address =
-  "0x5B82Fac52AB10807FAb0897a0ba05b26Eb50E684";
+  "0x8157075B278718C3c075043FAa85822Ad98d60F2";
 export const GMX_READER: Address = "0xf60becbba223EEA9495Da3f606753867eC10d139";
 export const DATA_STORE: Address = "0xFD70de6b91282D8017aA4E741e9Ae325CAb992d8";
 
