51858963db8b5696a10f5f7753e3b7572e6a64ba
Andrey <onipchenko.andrey.90@gmail.com>
2024-01-12 23:04:00 +0200

    deleted the old routes

--- src/router/index.js
+++ src/router/index.js
@@ -12,19 +12,9 @@ const routes = [
     component: () => import("@/views/Home.vue"),
   },
   {
-    path: "/tenderlyTap",
-    name: "TenderlyTap",
-    component: () => import("@/views/TenderlyTap.vue"),
-  },
-  {
-    path: "/borrow",
-    name: "Borrow",
-    component: () => import("@/views/borrow/Borrow.vue"),
-  },
-  {
-    path: "/borrow/:id?",
-    name: "BorrowId",
-    component: () => import("@/views/borrow/Borrow.vue"),
+    path: "/cauldrons",
+    name: "Cauldrons",
+    component: () => import("@/views/Cauldrons.vue"),
   },
   {
     path: "/market/:chainId/:cauldronId",
@@ -32,14 +22,25 @@ const routes = [
     component: () => import("@/views/Market.vue"),
   },
   {
-    path: "/repay",
-    name: "Repay",
-    component: () => import("@/views/borrow/Repay.vue"),
+    path: "/farms",
+    name: "MarketsFarm",
+    component: () => import("@/views/markets/Farms.vue"),
   },
   {
-    path: "/repay/:id",
-    name: "RepayId",
-    component: () => import("@/views/borrow/Repay.vue"),
+    path: "/farm/:id?/:farmChainId?",
+    name: "Farm",
+    component: () => import("@/views/Farm.vue"),
+    props: true,
+    beforeEnter: [removeQueryParams],
+  },
+  {
+    path: "/beam",
+    name: "Beam",
+    component: () => import("@/views/Beam.vue"),
+  },
+  {
+    path: "/bridge",
+    redirect: "/beam",
   },
   {
     path: "/spell",
@@ -62,45 +63,9 @@ const routes = [
     component: () => import("@/views/stake/MLVL.vue"),
   },
   {
-    path: "/leverage",
-    name: "Leverage",
-    component: () => import("@/views/borrow/Leverage.vue"),
-  },
-  {
-    path: "/leverage/:id",
-    name: "LeverageId",
-    component: () => import("@/views/borrow/Leverage.vue"),
-  },
-  {
-    path: "/deleverage",
-    name: "Deleverage",
-    component: () => import("@/views/borrow/Deleverage.vue"),
-  },
-  {
-    path: "/deleverage/:id",
-    name: "DeleverageId",
-    component: () => import("@/views/borrow/Deleverage.vue"),
-  },
-  {
-    path: "/beam",
-    name: "Beam",
-    component: () => import("@/views/Beam.vue"),
-  },
-  {
-    path: "/bridge",
-    redirect: "/beam",
-  },
-  {
-    path: "/farms",
-    name: "MarketsFarm",
-    component: () => import("@/views/markets/Farms.vue"),
-  },
-  {
-    path: "/farm/:id?/:farmChainId?",
-    name: "Farm",
-    component: () => import("@/views/Farm.vue"),
-    props: true,
-    beforeEnter: [removeQueryParams],
+    path: "/magicKLP",
+    name: "magicKLP",
+    component: () => import("@/views/stake/MKLP.vue"),
   },
   {
     path: "/my-positions",
@@ -108,19 +73,14 @@ const routes = [
     component: () => import("@/views/MyPositions.vue"),
   },
   {
-    path: "/cauldrons",
-    name: "Cauldrons",
-    component: () => import("@/views/Cauldrons.vue"),
+    path: "/tenderlyTap",
+    name: "TenderlyTap",
+    component: () => import("@/views/TenderlyTap.vue"),
   },
   {
     path: "/claim",
     name: "Claim",
     component: () => import("@/views/Claim.vue"),
-  },
-  {
-    path: "/magicKLP",
-    name: "magicKLP",
-    component: () => import("@/views/stake/MKLP.vue"),
   },
   {
     path: "/:catchAll(.*)",
