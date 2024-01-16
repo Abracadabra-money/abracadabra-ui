b535ec47e61a8a4745e685e990510bbdce0678a5
Andrey <onipchenko.andrey.90@gmail.com>
2024-01-16 11:52:50 +0200

    updated ClaimMimBlock test

--- src/components/stake/spell/ClaimMimBlock.test.ts
+++ src/components/stake/spell/ClaimMimBlock.test.ts
@@ -1,26 +1,71 @@
-import { describe, it, expect } from "vitest";
 import { shallowMount } from "@vue/test-utils";
+import { describe, it, expect, vi } from "vitest";
 import ClaimMimBlock from "@/components/stake/spell/ClaimMimBlock.vue";
+import Vuex from "vuex";
+import { ethers } from "ethers";
+
+const store = new Vuex.Store({
+  modules: {
+    connectProvider: {
+      state: { account: ethers.Wallet.createRandom() },
+      getters: {
+        getAccount: (state) => state.account,
+      },
+    },
+  },
+});
 
 describe("ClaimMimBlock", () => {
+  it("renders the correct button text when account is null and isUnsupportedChain is true", () => {
+    const store = new Vuex.Store({
+      modules: {
+        connectProvider: {
+          state: { account: null },
+          getters: {
+            getAccount: (state) => state.account,
+          },
+        },
+      },
+    });
+
+    const wrapper = shallowMount(ClaimMimBlock, {
+      propsData: {
+        claimAmount: "100",
+        isUnsupportedChain: true,
+      },
+      global: { plugins: [store] },
+      mocks: {
+        $emit: vi.fn(),
+      },
+    });
+
+    expect(wrapper.find(".claim-button").text()).toBe("Connect wallet");
+  });
+
   it("renders the correct button text when isUnsupportedChain is false", () => {
     const wrapper = shallowMount(ClaimMimBlock, {
       propsData: {
         claimAmount: "100",
         isUnsupportedChain: false,
-        isDisableClaimButton: true,
+      },
+      global: { plugins: [store] },
+      mocks: {
+        $emit: vi.fn(),
       },
     });
 
     expect(wrapper.find(".claim-button").text()).toBe("Switch Network");
   });
 
-  it("renders the correct button text when isUnsupportedChain is true", () => {
+  it("renders the correct button text when account is not null and isUnsupportedChain is true", () => {
     const wrapper = shallowMount(ClaimMimBlock, {
       propsData: {
         claimAmount: "100",
         isUnsupportedChain: true,
-        isDisableClaimButton: true,
+      },
+      global: { plugins: [store] },
+      mocks: {
+        $emit: vi.fn(),
       },
     });
 
@@ -32,7 +77,10 @@ describe("ClaimMimBlock", () => {
       propsData: {
         claimAmount: "1000000000000000000",
         isUnsupportedChain: true,
-        isDisableClaimButton: true,
+      },
+      global: { plugins: [store] },
+      mocks: {
+        $emit: vi.fn(),
       },
     });
 
