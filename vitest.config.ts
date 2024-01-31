6a8d4680e584a1c787388dfd0c07c9b6b7629a08
Andrey <onipchenko.andrey.90@gmail.com>
2024-01-31 16:41:41 +0200

    updated vitest.config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,5 @@
-import { fileURLToPath } from "node:url";
-import { mergeConfig } from "vite";
+import { fileURLToPath, resolve } from "node:url";
+import { mergeConfig, UserConfig } from "vite";
 import { configDefaults, defineConfig } from "vitest/config";
 import viteConfig from "./vite.config";
 import vue from "@vitejs/plugin-vue";
@@ -27,6 +27,12 @@ export default mergeConfig(
       environment: "jsdom",
       exclude: [...configDefaults.exclude, "e2e/*"],
       root: fileURLToPath(new URL("./", import.meta.url)),
+      setupFiles: [
+        resolve(
+          fileURLToPath(new URL("./src/", import.meta.url)),
+          "test/setup.ts"
+        ),
+      ],
     },
   })
 );
