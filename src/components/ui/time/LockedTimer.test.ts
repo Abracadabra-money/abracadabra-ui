2ec271025e7fb40b4bd2de5a54fe0597d5fc0b83
Andrey <onipchenko.andrey.90@gmail.com>
2024-01-31 17:42:03 +0200

    added unit test for tooltips folder

--- src/components/ui/time/LockedTimer.test.ts
+++ src/components/ui/time/LockedTimer.test.ts
@@ -1,6 +1,6 @@
 import { mount } from "@vue/test-utils";
 import { describe, it, expect } from "vitest";
-import LockedTimer from "./LockedTimer.vue";
+import LockedTimer from "@/components/ui/time//LockedTimer.vue";
 
 describe("LockedTimer", () => {
   it("displays the correct countdown", async () => {
