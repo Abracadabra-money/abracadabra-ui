bca80d2d182c8b842959697bc80b4e739b0aa51e
5450L <susalafamilia@gmail.com>
2024-02-20 16:07:24 +0200

    Added fetching total supply on disconnect

--- src/helpers/stake/spell/emptyState.ts
+++ src/helpers/stake/spell/emptyState.ts
@@ -4,6 +4,8 @@ import { spellConfig } from "@/configs/stake/spellConfig";
 import type { EmptySpellState, EmptyTokenState } from "@/types/spell/empyState";
 import { getSpellToSSpellRate } from "@/helpers/stake/spell/getSpellToSSpellRate";
 import { getSpellStakingApr } from "./getSpellStakingApr";
+import { createPublicClient, http } from "viem";
+import { chainsList } from "@/helpers/chains/index";
 
 const config = spellConfig[1 as keyof typeof spellConfig];
 const { spell, sSpell, mSpell }: any = config;
@@ -17,18 +19,19 @@ const spellEmptyState: EmptyTokenState = {
   decimals: 18,
 };
 
-const mSpellEmptyState: EmptyTokenState = {
-  icon: mSpell.icon,
-  name: mSpell.name,
-  balance: 0n,
-  claimableAmount: 0n,
-  price: ONE_ETHER_VIEM,
-  rate: ONE_ETHER_VIEM,
-  decimals: 18,
-};
-
 export const getSSpellEmptyState = async (): Promise<EmptyTokenState> => {
   const spellToSSpellRate = await getSpellToSSpellRate(spell, sSpell.contract);
+
+  const publicClient = createPublicClient({
+    chain: chainsList[1],
+    transport: http(),
+  });
+
+  const totalSupply: any = await publicClient.readContract({
+    ...sSpell.contract,
+    functionName: "totalSupply",
+    args: [],
+  })
 
   return {
     icon: sSpell?.icon || useImage("assets/images/sspell-icon.svg"),
@@ -38,6 +41,40 @@ export const getSSpellEmptyState = async (): Promise<EmptyTokenState> => {
     rate: spellToSSpellRate,
     price: ONE_ETHER_VIEM,
     decimals: 18,
+    totalSupply,
+  };
+};
+
+export const getMSpellEmptyState = async (chainId: number): Promise<EmptyTokenState> => {
+
+  const publicClient = createPublicClient({
+    chain: chainsList[chainId as keyof typeof chainsList],
+    transport: http(),
+  });
+  const mSpellContract = spellConfig[chainId as keyof typeof spellConfig].mSpell.contract
+
+  const spellAddress: any = await publicClient.readContract({
+    ...mSpellContract,
+    functionName: "spell",
+    args: [],
+  });
+
+  const totalSupply: any = await publicClient.readContract({
+    address: spellAddress,
+    abi: spell.abi,
+    functionName: "totalSupply",
+    args: [],
+  })
+
+  return {
+    icon: mSpell.icon,
+    name: mSpell.name,
+    balance: 0n,
+    claimableAmount: 0n,
+    price: ONE_ETHER_VIEM,
+    rate: ONE_ETHER_VIEM,
+    decimals: 18,
+    totalSupply
   };
 };
 
@@ -45,7 +82,8 @@ export const getSpellEmptyState = async (
   chainId: number
 ): Promise<EmptySpellState> => {
   const sSpell = await getSSpellEmptyState();
-
+  const mSpellEmptyState = await getMSpellEmptyState(chainId)
+  
   const { sSpellApr, mSpellApr } = await getSpellStakingApr();
 
   return {
