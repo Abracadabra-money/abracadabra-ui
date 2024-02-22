4a0effec88d659e7925d583826ac37ea994e731d
0xhardrefresh <0xhardrefresh@protonmail.com>
2024-02-22 22:54:04 +0200

    test

--- middleware.js
+++ middleware.js
@@ -1,6 +1,8 @@
-import { next } from "@vercel/edge";
+import { next, ipAddress } from "@vercel/edge";
 
 export default async function middleware(request) {
+  const ip = ipAddress(request);
   console.log("im alive");
+  console.log("ip is:", ip)
   next();
 }
