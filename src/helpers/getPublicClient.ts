dcd0b929c70fff3aa4061bd3d99997aeabeb6d38
Andrey <onipchenko.andrey.90@gmail.com>
2024-02-22 17:55:03 +0200

    updated helper getPublicClient

--- src/helpers/getPublicClient.ts
+++ src/helpers/getPublicClient.ts
@@ -2,9 +2,9 @@ import { createPublicClient, http } from "viem";
 import type { PublicClient } from "@/types/global";
 import { getChainById } from "@/helpers/chains/index";
 
-export const getPublicClient = (chainId: number): PublicClient => {
+export const getPublicClient = (chainId: number | string): PublicClient => {
   return createPublicClient({
-    chain: getChainById(chainId),
+    chain: getChainById(Number(chainId)),
     transport: http(),
   });
 };
