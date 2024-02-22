a00d8cc73696788bd870cd219d951f4b472ba05b
0xhardrefresh <0xhardrefresh@protonmail.com>
2024-02-22 23:00:04 +0200

    test

--- middleware.js
+++ middleware.js
@@ -1,8 +1,22 @@
 import { next, ipAddress } from "@vercel/edge";
+import { Ratelimit } from '@upstash/ratelimit'
+import { kv } from '@vercel/kv'
+
+const ratelimit = new Ratelimit({
+    redis: kv,
+    // 5 requests from the same IP in 10 seconds
+    limiter: Ratelimit.slidingWindow(5, '10 s'),
+  })
 
 export default async function middleware(request) {
   const ip = ipAddress(request);
-  console.log("im alive");
   console.log("ip is:", ip)
+
+  const { success, pending, limit, reset, remaining } = await ratelimit.limit(
+    ip
+  )
+
+  console.log("limit", success)
+
   next();
 }
