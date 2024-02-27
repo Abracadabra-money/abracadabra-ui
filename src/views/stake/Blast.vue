63e65ec54572cfd29b5ecb8577b0565d905f31a7
Andrey <onipchenko.andrey.90@gmail.com>
2024-02-27 22:34:40 +0200

    added Blast view and ActionBlock

---
+++ src/views/stake/Blast.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="stake-view">
+    <div class="stake-wrap" v-if="stakeInfo">
+      <ActionBlock :stakeInfo="stakeInfo" />
+    </div>
+
+    <div class="loader-wrap" v-else>
+      <BaseLoader large text="Loading stake" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineAsyncComponent } from "vue";
+import { mapGetters, mapActions, mapMutations } from "vuex";
+import { getStakeInfo } from "@/helpers/blast/stake/getStakeInfo";
+
+export default {
+  data() {
+    return {
+      stakeInfo: null as any,
+      updateInterval: null as any,
+    };
+  },
+
+  computed: {
+    ...mapGetters({ accaunt: "getAccount" }),
+  },
+
+  methods: {
+    ...mapActions({ createNotification: "notifications/new" }),
+    ...mapMutations({ deleteNotification: "notifications/delete" }),
+    async createStakeInfo() {
+      this.stakeInfo = await getStakeInfo(this.accaunt);
+    },
+  },
+
+  async created() {
+    await this.createStakeInfo();
+
+    this.updateInterval = setInterval(async () => {
+      await this.createStakeInfo();
+    }, 60000);
+  },
+
+  beforeUnmount() {
+    clearInterval(this.updateInterval);
+  },
+
+  components: {
+    ActionBlock: defineAsyncComponent(
+      () => import("@/components/stake/earnPoints/Actionsblock.vue")
+    ),
+    BaseLoader: defineAsyncComponent(
+      () => import("@/components/base/BaseLoader.vue")
+    ),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.stake-view {
+  min-height: 100vh;
+}
+
+.stake-wrap {
+  position: relative;
+  max-width: 1310px;
+  width: 100%;
+  padding: 124px 15px 90px;
+  display: grid;
+  grid-template-columns: 520px 1fr;
+  grid-gap: 24px;
+  margin: 0 auto;
+}
+
+.loader-wrap {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+@media screen and (max-width: 1200px) {
+  .stake-wrap {
+    grid-template-columns: 400px 1fr;
+  }
+}
+
+@media screen and (max-width: 1024px) {
+  .stake-wrap {
+    grid-template-columns: 100%;
+    grid-template-rows: auto;
+  }
+}
+</style>
