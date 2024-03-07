ad1c0c9e7dbae58ae3f6de221a9d2e0c76cd4bd9
Andrey <onipchenko.andrey.90@gmail.com>
2024-03-07 17:43:48 +0200

    updated action addLiquidity and removeLiquidity

--- src/helpers/blast/swap/actions/addLiquidity.ts
+++ src/helpers/blast/swap/actions/addLiquidity.ts
@@ -3,24 +3,27 @@ import { waitForTransaction } from "@wagmi/core";
 import BlastMIMSwapRouterAbi from "@/abis/BlastMIMSwapRouter";
 import { prepareWriteContract, writeContract } from "@wagmi/core";
 
-export type AddLiquidityPayload = [
-  lp: Address,
-  to: Address,
-  baseInAmount: bigint,
-  quoteInAmount: bigint,
-  minimumShares: bigint,
-  deadline: bigint
-];
+export type AddLiquidityPayload = {
+  lp: Address;
+  to: Address;
+  baseInAmount: bigint;
+  quoteInAmount: bigint;
+  minimumShares: bigint;
+  deadline: bigint;
+};
 
 export const addLiquidity = async (
   swapRouterAddress: Address,
   payload: AddLiquidityPayload
 ) => {
+  const { lp, to, baseInAmount, quoteInAmount, minimumShares, deadline } =
+    payload;
+
   const config = await prepareWriteContract({
     address: swapRouterAddress,
     abi: BlastMIMSwapRouterAbi,
     functionName: "addLiquidity",
-    args: [...payload],
+    args: [lp, to, baseInAmount, quoteInAmount, minimumShares, deadline],
   });
 
   const { hash } = await writeContract(config);
--- src/helpers/blast/swap/actions/removeLiquidity.ts
+++ src/helpers/blast/swap/actions/removeLiquidity.ts
@@ -3,24 +3,27 @@ import { waitForTransaction } from "@wagmi/core";
 import BlastMIMSwapRouterAbi from "@/abis/BlastMIMSwapRouter";
 import { prepareWriteContract, writeContract } from "@wagmi/core";
 
-export type RemoveLiquidityPayload = [
-  lp: Address,
-  to: Address,
-  sharesIn: bigint,
-  minimumBaseAmount: bigint,
-  minimumQuoteAmount: bigint,
-  deadline: bigint
-];
+export type RemoveLiquidityPayload = {
+  lp: Address;
+  to: Address;
+  sharesIn: bigint;
+  minimumBaseAmount: bigint;
+  minimumQuoteAmount: bigint;
+  deadline: bigint;
+};
 
 export const removeLiquidity = async (
   swapRouterAddress: Address,
   payload: RemoveLiquidityPayload
 ) => {
+  const { lp, to, sharesIn, minimumBaseAmount, minimumQuoteAmount, deadline } =
+    payload;
+
   const config = await prepareWriteContract({
     address: swapRouterAddress,
     abi: BlastMIMSwapRouterAbi,
     functionName: "removeLiquidity",
-    args: [...payload],
+    args: [lp, to, sharesIn, minimumBaseAmount, minimumQuoteAmount, deadline],
   });
 
   const { hash } = await writeContract(config);
