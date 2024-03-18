191c9a7d823065adacb3635b21ff5e04811a8883
Andrey <onipchenko.andrey.90@gmail.com>
2024-03-18 12:14:47 +0200

    updated BlastMIMSwapRouter abi, added balanceOf

--- src/abis/BlastMIMSwapRouter.js
+++ src/abis/BlastMIMSwapRouter.js
@@ -811,6 +811,13 @@ export default [
     type: "function",
   },
   {
+    inputs: [{ internalType: "address", name: "", type: "address" }],
+    name: "balanceOf",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
     stateMutability: "payable",
     type: "receive",
   },
