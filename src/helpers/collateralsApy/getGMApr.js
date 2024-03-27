bcd7f46540898318b56644dfb694a9b644e30ccb
0xhardrefresh <98540076+0xhardrefresh@users.noreply.github.com>
2024-03-27 14:24:48 +0200

    remove incentiveApr from GLP apr calc (#345)

--- src/helpers/collateralsApy/getGMApr.js
+++ src/helpers/collateralsApy/getGMApr.js
@@ -1,20 +1,20 @@
 import { getMarketsApr } from "../gm/apr/getMarketApr";
 import { utils, BigNumber } from "ethers";
 
-export const ABRA_FEES = 1000; // 10% 
+export const ABRA_FEES = 1000; // 10%
 export const BASIS_POINTS_DIVISOR = 10000;
 
 export const getGMApr = async (market, provider) => {
   const marketsApr = await getMarketsApr(provider);
   const aprData = BigNumber.from(marketsApr.marketsTokensAPRData[market]);
-  const incentiveAprData = BigNumber.from(marketsApr.marketsTokensIncentiveAprData[market]);
+  // const incentiveAprData = BigNumber.from(marketsApr.marketsTokensIncentiveAprData[market]);
 
-  const feesBasicPoints = BASIS_POINTS_DIVISOR - ABRA_FEES
-  const bonusApr = incentiveAprData
-    .mul(feesBasicPoints)
-    .div(BASIS_POINTS_DIVISOR)
+  // const feesBasicPoints = BASIS_POINTS_DIVISOR - ABRA_FEES;
+  // const bonusApr = incentiveAprData
+  //   .mul(feesBasicPoints)
+  //   .div(BASIS_POINTS_DIVISOR)
 
-  const apr = aprData.add(bonusApr);
+  // const apr = aprData.add(bonusApr);
 
-  return utils.formatUnits(apr, 2);
+  return utils.formatUnits(aprData, 2);
 };
