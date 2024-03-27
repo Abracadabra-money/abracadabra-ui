a8d9dbef06c47594b85faf3b742f3f0f1ed4b5b8
5450L <susalafamilia@gmail.com>
2024-03-27 19:05:56 +0200

    PoolPosition rocket tooltip text changed

--- src/components/pools/pool/LockBlock.vue
+++ src/components/pools/pool/LockBlock.vue
@@ -27,7 +27,12 @@
 
     <div class="info-item gold">
       <p>Founder boost</p>
-      <div class="icon-wrap" v-tooltip="'Boosted Airdrop for Founders'">
+      <div
+        class="icon-wrap"
+        v-tooltip="
+          'Receiving 20% of total ecosystem points: Points, Gold, Potions'
+        "
+      >
         <img src="@/assets/images/points-dashboard/rocket.svg" alt="" />
       </div>
     </div>
