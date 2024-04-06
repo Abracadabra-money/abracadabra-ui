6aa55c9e001226f87caeefc1f148fc3f99ab8c3f
Andrey <onipchenko.andrey.90@gmail.com>
2024-04-06 14:34:30 +0300

    updated swap helper action addLiquidityETH

--- src/helpers/pools/swap/actions/addLiquidityETH.ts
+++ src/helpers/pools/swap/actions/addLiquidityETH.ts
@@ -1,7 +1,10 @@
+import {
+  writeContractHelper,
+  simulateContractHelper,
+  waitForTransactionReceiptHelper,
+} from "@/helpers/walletClienHelper";
 import type { Address } from "viem";
-import { waitForTransaction } from "@wagmi/core";
 import BlastMIMSwapRouterAbi from "@/abis/BlastMIMSwapRouter";
-import { prepareWriteContract, writeContract } from "@wagmi/core";
 
 export type AddLiquidityETHPayload = {
   payableAmount: bigint;
@@ -27,7 +30,7 @@ export const addLiquidityETH = async (
     deadline,
   } = payload;
 
-  const config = await prepareWriteContract({
+  const { request } = await simulateContractHelper({
     address: swapRouterAddress,
     abi: BlastMIMSwapRouterAbi,
     functionName: "addLiquidityETH",
@@ -35,6 +38,7 @@ export const addLiquidityETH = async (
     value: payableAmount,
   });
 
-  const { hash } = await writeContract(config);
-  return await waitForTransaction({ hash });
+  const hash = await writeContractHelper(request);
+
+  return await waitForTransactionReceiptHelper({ hash });
 };
