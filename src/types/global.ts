0111542069399da818e0c52e4740df5750e6541c
Andrey <onipchenko.andrey.90@gmail.com>
2024-04-24 13:46:48 +0300

    refactoring caldron list (viem)

--- src/types/global.ts
+++ src/types/global.ts
@@ -4,3 +4,11 @@ export type ContractInfo = {
   address: Address;
   abi: any;
 };
+
+export type RouterLinkParams = {
+  name: string;
+  params?: {
+    chainId?: number;
+    cauldronId?: number;
+  };
+};
