733e2b3c76ca6bbec86683dcf7720476128b7564
0xhardrefresh <0xhardrefresh@protonmail.com>
2024-06-18 17:23:45 +0300

    added permit helper

--- src/constants/blastLpMigration.ts
+++ src/constants/blastLpMigration.ts
@@ -1,5 +1,11 @@
-export const BLAST_BRIDGE_ADDRESS = "0xDa47C2662ce5773ec25c7C6Bfb149ec7bFEeE69D";
-export const ARB_BRIDGE_ADDRESS = "0xDa47C2662ce5773ec25c7C6Bfb149ec7bFEeE69D";
-export const LOCK_CONTRACT_ADDRESS = "0xf1ed28fa139f2df5cf3ed140aa9f803c79554519";
+import type { Address } from "viem";
 
-export const MAGIC_LP_ADDRESS = "0x163B234120aaE59b46b228d8D88f5Bc02e9baeEa";
+export const BLAST_BRIDGE_ADDRESS: Address =
+  "0xDa47C2662ce5773ec25c7C6Bfb149ec7bFEeE69D";
+export const ARB_BRIDGE_ADDRESS: Address =
+  "0xDa47C2662ce5773ec25c7C6Bfb149ec7bFEeE69D";
+export const LOCK_CONTRACT_ADDRESS: Address =
+  "0xf1ed28fa139f2df5cf3ed140aa9f803c79554519";
+
+export const MAGIC_LP_ADDRESS: Address =
+  "0x163B234120aaE59b46b228d8D88f5Bc02e9baeEa";
