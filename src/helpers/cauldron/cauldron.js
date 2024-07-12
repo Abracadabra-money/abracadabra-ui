905e05d18a7148db15318d97d10820f33dcdce0e
Andrey <onipchenko.andrey.90@gmail.com>
2024-07-12 14:19:13 +0300

    added cookTenderly

--- src/helpers/cauldron/cauldron.js
+++ src/helpers/cauldron/cauldron.js
@@ -3,6 +3,13 @@ import {
   simulateContractHelper,
   waitForTransactionReceiptHelper,
 } from "@/helpers/walletClienHelper";
+import store from "@/store";
+import { Contract } from "ethers";
+import { createWalletClient, custom } from "viem";
+import { getAccountHelper } from "@/helpers/walletClienHelper";
+import { getChainConfig } from "@/helpers/chains/getChainsInfo";
+import { getPublicClient } from "@/helpers/chains/getChainsInfo";
+import { checkUseTenderlyFork } from "@/helpers/tenderly/checkUseTenderlyFork";
 
 export const cook = async (contract, cookData, value) => {
   const estimateGas = await contract.estimateGas.cook(
@@ -30,7 +37,9 @@ export const cook = async (contract, cookData, value) => {
 };
 
 export const cookViem = async (cauldronObject, cookData, value) => {
-  console.log("cauldronObject", cauldronObject)
+  const useForkRpc = checkUseTenderlyFork(cauldronObject.config.chainId);
+
+  if (useForkRpc) return cookTenderly(cauldronObject, cookData, value);
 
   const { request } = await simulateContractHelper({
     address: cauldronObject.config.contract.address,
@@ -43,4 +52,50 @@ export const cookViem = async (cauldronObject, cookData, value) => {
   const hash = await writeContractHelper(request);
 
   return await waitForTransactionReceiptHelper({ hash });
+};
+
+const cookTenderly = async (cauldronObject, cookData, value) => {
+  try {
+    const account = getAccountHelper();
+    const chainConfig = getChainConfig(cauldronObject.config.chainId);
+    const publicClient = getPublicClient(cauldronObject.config.chainId);
+
+    const walletClient = createWalletClient({
+      chain: chainConfig.viemConfig,
+      transport: custom(publicClient),
+    });
+
+    const { request } = await publicClient.simulateContract({
+      account: account,
+      address: cauldronObject.config.contract.address,
+      abi: cauldronObject.config.contract.abi,
+      functionName: "cook",
+      args: [cookData.events, cookData.values, cookData.datas],
+      value,
+    });
+
+    return await walletClient.writeContract(request);
+  } catch (error) {
+    console.log("error");
+
+    const signer = store.getters.getSigner;
+
+    const cauldronContract = new Contract(
+      cauldronObject.config.contract.address,
+      cauldronObject.config.contract.abi,
+      signer
+    );
+
+    const tx = await cauldronContract.cook(
+      cookData.events,
+      cookData.values,
+      cookData.datas,
+      {
+        value,
+        gasLimit: 5000000,
+      }
+    );
+
+    return await tx.wait();
+  }
 };
