eaf1a1428bd405a7e3dfffca2d9c21de86108ac5
Андрей <47685068+Andrey-Onipchenko@users.noreply.github.com>
2024-09-03 17:01:56 +0300

    ✨ sdeUSD v2 & swappers update (#449)
    * added signature validation
    
    * show console
    
    * integrate 0x v2 [test]
    
    * updated swap0xRequestV2 structure
    
    * update sdeUSD leverage logic & contract
    
    * added sdeUSD delev swapper
    
    * added liquidity error
    
    * added error to leverage
    
    * remove logs
    
    * hide logs
    
    * added new cauldron sdeUSD V2
    
    * updated tokens icon
    
    * updated MIM / deUSD pool config. Added stake contract
    
    * added migration banner
    
    * added topup timer to marked page
    
    * added render condition to timer
    
    * added elixir rewards
    
    * fixed empty position case
    
    * fixes
    
    * Pool: remove isArbitrumMimUsdcPool condition
    
    * updated pool card
    
    * deleted icon
    
    * fixed cauldron emptyPosition
    
    ---------
    
    Co-authored-by: 0xhardrefresh <0xhardrefresh@protonmail.com>
    Co-authored-by: 0xhardrefresh <98540076+0xhardrefresh@users.noreply.github.com>
    Co-authored-by: Andrey <onipchenko.andrey.90@gmail.com>

---
+++ src/components/pools/pool/ElixirPotions.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="elixir-potions">
+    <span>Stake to Earn Elixir Potions</span>
+    <span>5x Multiplier </span>
+  </div>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.elixir-potions {
+  width: 100%;
+  display: flex;
+  font-weight: 500;
+  justify-content: space-between;
+  padding: 12px;
+  align-items: center;
+  border-radius: 12px;
+  background: linear-gradient(
+      0deg,
+      rgba(0, 0, 0, 0.2) 0%,
+      rgba(0, 0, 0, 0.2) 100%
+    ),
+    linear-gradient(270deg, #ffe47c -3.8%, #ff43c3 50.8%, #4156e0 100%),
+    linear-gradient(
+      146deg,
+      rgba(0, 10, 35, 0.07) 0%,
+      rgba(0, 80, 156, 0.07) 101.49%
+    );
+  box-shadow: 0px 4px 33px 0px rgba(0, 0, 0, 0.06);
+}
+
+@media screen and (max-width: 600px) {
+  .elixir-potions {
+    flex-direction: column;
+    gap: 4px;
+  }
+}
+</style>
