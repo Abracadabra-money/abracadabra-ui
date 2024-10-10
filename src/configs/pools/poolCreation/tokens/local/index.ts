ec86b93ba868d69245ac63741b524a4fea00b768
5450L <susalafamilia@gmail.com>
2024-10-10 12:11:29 +0300

    fixed blastTokensI import

--- src/configs/pools/poolCreation/tokens/local/index.ts
+++ src/configs/pools/poolCreation/tokens/local/index.ts
@@ -1,6 +1,6 @@
 import arbitrumTokens from "@/configs/pools/poolCreation/tokens/local/arbitrumTokens";
 import ethereumTokens from "@/configs/pools/poolCreation/tokens/local/ethereumTokens";
-import blastTokens from "@/configs/pools/poolCreation/tokens/local/ethereumTokens";
+import blastTokens from "@/configs/pools/poolCreation/tokens/local/blastTokens";
 import kavaTokens from "@/configs/pools/poolCreation/tokens/local/kavaTokens";
 
 export default [
