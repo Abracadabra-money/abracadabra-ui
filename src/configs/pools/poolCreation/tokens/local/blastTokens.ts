97e6b5db8ab19b22213c10c7db084026a18f0592
5450L <susalafamilia@gmail.com>
2024-10-29 17:16:17 +0200

    blast token list for pool creation updated

--- src/configs/pools/poolCreation/tokens/local/blastTokens.ts
+++ src/configs/pools/poolCreation/tokens/local/blastTokens.ts
@@ -22,6 +22,76 @@ const blastTokens: Array<PoolCreationTokenConfig> = [
     decimals: 18,
     abi: erc20Abi,
   },
+  {
+    chainId: BLAST_CHAIN_ID,
+    address: "0xc7Edf7B7b3667a06992508e7B156eff794a9e1c8",
+    name: "XPRT native asset",
+    symbol: "XPRT",
+    //todo new token icon
+    icon: useImage("assets/images/base_token_icon.png"),
+    decimals: 6,
+    abi: erc20Abi,
+  },
+  {
+    chainId: BLAST_CHAIN_ID,
+    address: "0xE36072DD051Ce26261BF50CD966311cab62C596e",
+    name: "Thruster",
+    symbol: "THRUST",
+    //todo new token icon
+    icon: useImage("assets/images/base_token_icon.png"),
+    decimals: 18,
+    abi: erc20Abi,
+  },
+  {
+    chainId: BLAST_CHAIN_ID,
+    address: "0x818a92bc81Aad0053d72ba753fb5Bc3d0C5C0923",
+    name: "Juice",
+    symbol: "JUICE",
+    //todo new token icon
+    icon: useImage("assets/images/base_token_icon.png"),
+    decimals: 18,
+    abi: erc20Abi,
+  },
+  {
+    chainId: BLAST_CHAIN_ID,
+    address: "0x15bD262ede6E8aA04b2361a1Df697adF1Cf40e75",
+    name: "Petoshi",
+    symbol: "PETOSHI",
+    //todo new token icon
+    icon: useImage("assets/images/base_token_icon.png"),
+    decimals: 18,
+    abi: erc20Abi,
+  },
+  {
+    chainId: BLAST_CHAIN_ID,
+    address: "0x5ffd9EbD27f2fcAB044c0f0a26A45Cb62fa29c06",
+    name: "PacMoon",
+    symbol: "PAC",
+    //todo new token icon
+    icon: useImage("assets/images/base_token_icon.png"),
+    decimals: 18,
+    abi: erc20Abi,
+  },
+  {
+    chainId: BLAST_CHAIN_ID,
+    address: "0x9FE9991dAF6b9a5d79280F48cbb6827D46DE2EA4",
+    name: "HyperBlast",
+    symbol: "HYPE",
+    //todo new token icon
+    icon: useImage("assets/images/base_token_icon.png"),
+    decimals: 9,
+    abi: erc20Abi,
+  },
+  {
+    chainId: BLAST_CHAIN_ID,
+    address: "0xb1a5700fA2358173Fe465e6eA4Ff52E36e88E2ad",
+    name: "Blast",
+    symbol: "BLAST",
+    //todo new token icon
+    icon: useImage("assets/images/base_token_icon.png"),
+    decimals: 18,
+    abi: erc20Abi,
+  },
 ];
 
 export default blastTokens;
