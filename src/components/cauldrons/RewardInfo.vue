91ea18a3f6e61b66a701ad2ede667b6008a5f2d8
Andrey <onipchenko.andrey.90@gmail.com>
2024-11-15 10:47:44 +0200

    Updated elixir reward info

--- src/components/cauldrons/RewardInfo.vue
+++ src/components/cauldrons/RewardInfo.vue
@@ -1,5 +1,26 @@
 <template>
-  <div :class="['reward-info', { small }]" v-if="isPillsPoints">
+  <div :class="['reward-info', { small }]" v-if="isMultiplierLabel">
+    <div class="elixir-info">
+      <div class="elixir-multiplier">
+        <span> 5x - 29.5x</span>
+        <img
+          class="elixir-icon"
+          src="@/assets/images/get-lp-icons/elixir.png"
+          alt="Elixir icon"
+        />
+      </div>
+      <div class="elixir-apr">
+        {{ loopApr }}
+        <img
+          class="token-icon"
+          src="@/assets/images/tokens/sdeUSD.png"
+          alt="sdeUSD icon"
+        />
+      </div>
+    </div>
+  </div>
+
+  <div :class="['reward-info', { small }]" v-else-if="isPillsPoints">
     <div class="additional-info">
       <div class="additional-info-title">Pills Multiplier</div>
       <div class="additional-info-value">1x - 6.3x</div>
@@ -31,11 +52,19 @@ export default {
 
   data() {
     return {
+      elixirPotions: [43, 44],
       pillsPoints: [45],
     };
   },
 
   computed: {
+    isMultiplierLabel() {
+      return (
+        this.cauldron.config.chainId === MAINNET_CHAIN_ID &&
+        this.elixirPotions.includes(this.cauldron.config.id)
+      );
+    },
+
     isPillsPoints() {
       return (
         this.cauldron.config.chainId === MAINNET_CHAIN_ID &&
@@ -72,14 +101,37 @@ export default {
   padding: 5px 18px;
 }
 
-.column {
-  max-width: 180px;
-  width: 100%;
-}
-
 .small {
   max-width: initial;
   width: initial;
+
+  .additional-info-value,
+  .apr {
+    font-size: 14px;
+  }
+}
+
+.elixir-info {
+  gap: 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.elixir-multiplier,
+.elixir-apr {
+  text-shadow: 0px 0px 16px #ab5de8;
+  font-weight: 600;
+  gap: 6px;
+  display: flex;
+  align-items: center;
+}
+
+.elixir-icon,
+.token-icon {
+  width: 20px;
+  height: 20px;
 }
 
 .additional-info {
@@ -103,22 +155,25 @@ export default {
   line-height: 16px;
 }
 
+.column {
+  max-width: 180px;
+  width: 100%;
+}
+
 .apr {
   text-shadow: 0px 0px 16px #ab5de8;
   font-weight: 600;
   line-height: 150%;
 }
 
-.small {
-  .additional-info-value,
-  .apr {
-    font-size: 14px;
-  }
-}
-
 @media screen and (max-width: 600px) {
   .reward-info {
     padding: 5px 8px;
+  }
+
+  .elixir-multiplier,
+  .elixir-apr {
+    font-size: 12px;
   }
 
   .additional-info-title {
