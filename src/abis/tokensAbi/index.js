f8d7fe1ad711ce4a64a27ca7c70545b270b8b8b9
Andrey <onipchenko.andrey.90@gmail.com>
2024-11-18 12:18:58 +0200

    added Ena Cauldron

--- src/abis/tokensAbi/index.js
+++ src/abis/tokensAbi/index.js
@@ -53,6 +53,7 @@ import MagicKodiakVault from "@/abis/tokensAbi/MagicKodiakVault";
 import MagicUSD0pp from "@/abis/tokensAbi/MagicUSD0pp";
 import USD0pp from "@/abis/tokensAbi/USD0pp";
 import ZRO from "@/abis/tokensAbi/ZRO";
+import ENA from "@/abis/tokensAbi/ENA";
 
 export default {
   ALCX,
@@ -109,5 +110,6 @@ export default {
   MagicKodiakVault,
   MagicUSD0pp,
   USD0pp,
-  ZRO
+  ZRO,
+  ENA,
 };
