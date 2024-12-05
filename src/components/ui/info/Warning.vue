3ef3f411a03771431a4cc0f2177d2c73de281959
5450L <susalafamilia@gmail.com>
2024-12-05 15:36:02 +0200

    sdeusd deleverage warning added

--- src/components/ui/info/Warning.vue
+++ src/components/ui/info/Warning.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="warning-wrap">
     <img
-      class="error-image"
+      class="warning-image"
+      :styles="warningImageStyles"
       src="@/assets/images/pools/pool-creation/warning-cross.svg"
-      alt="Error"
+      alt="Warning"
+      v-if="mark === WarningIcon.Cross"
+    />
+    <ExclamationMarkIcon
+      :size="markSize"
+      color="rgba(254, 216, 79, 1)"
+      v-if="mark === WarningIcon.Exclamation"
     />
     <p class="warning-text">
       <slot />
@@ -12,32 +19,64 @@
 </template>
 
 <script lang="ts">
-export default {};
+import { defineAsyncComponent } from "vue";
+
+enum WarningIcon {
+  Cross = "cross",
+  Exclamation = "exclamation",
+}
+export default {
+  props: {
+    mark: {
+      type: String,
+      default: WarningIcon.Cross,
+    },
+    markSize: { type: String, default: "28" },
+  },
+
+  data() {
+    return {
+      WarningIcon,
+    };
+  },
+
+  computed: {
+    warningImageStyles() {
+      return `
+      height: ${this.markSize};
+      width: ${this.markSize};
+      `;
+    },
+  },
+
+  components: {
+    ExclamationMarkIcon: defineAsyncComponent(
+      () => import("@/components/ui/icons/ExclamationMarkIcon.vue")
+    ),
+  },
+};
 </script>
 <style lang="scss" scoped>
 .warning-wrap {
   display: flex;
   align-items: center;
-  gap: 12px;
+  gap: 4px;
   width: 100%;
-  padding: 16px 12px;
   border-radius: 16px;
-  border: 1px solid #c39818;
-  background: rgba(195, 152, 24, 0.1);
+  border: 1px solid #fed84f;
+  background: linear-gradient(
+      0deg,
+      rgba(195, 152, 24, 0.1) 0%,
+      rgba(195, 152, 24, 0.1) 100%
+    ),
+    rgba(16, 22, 34, 0.4);
+
   box-shadow: 0px 4px 32px 0px rgba(103, 103, 103, 0.14);
   backdrop-filter: blur(12.5px);
 }
 
 .warning-text {
-  color: rgba(255, 255, 255, 0.6);
   font-size: 14px;
   font-weight: 400;
-}
-
-@media (max-width: 600px) {
-  .warning-wrap {
-    align-items: flex-start;
-    padding: 7px 12px;
-  }
 }
 </style>
