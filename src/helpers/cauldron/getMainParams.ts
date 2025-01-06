3afe9675dc74e14ce161dddb691b180e6e211472
0xmDreamy <0xmDreamy@proton.me>
2025-01-06 14:30:25 +0100

    split getMainParams

--- src/helpers/cauldron/getMainParams.ts
+++ src/helpers/cauldron/getMainParams.ts
@@ -1,5 +1,5 @@
 import { BigNumber } from "ethers";
-import { type Address, type ReadContractErrorType, type PublicClient, erc4626Abi } from "viem";
+import { type Address, type PublicClient, erc4626Abi } from "viem";
 import lensAbi from "@/abis/marketLens";
 import type { MainParams } from "@/helpers/cauldron/types";
 import { getPublicClient } from "@/helpers/chains/getChainsInfo";
@@ -52,114 +52,112 @@ export const getMainParams = async (
   cauldron?: CauldronContractConfig | undefined
 ): Promise<Array<MainParams>> => {
   const lensAddress = getLensAddress(chainId);
-  const publicClient = getPublicClient(chainId);
+  const publicClient: PublicClient = getPublicClient(chainId);
 
-  const marketInfos = await (publicClient.multicall({
-    contracts: configs.map((config) => {
-      const methodName =
-        config.version === 2
-          ? `getMarketInfoCauldronV2${config.cauldronSettings.oracleInfo?.kind === "PYTH" ? "Pyth" : ""}`
-          : `getMarketInfoCauldronV3${config.cauldronSettings.oracleInfo?.kind === "PYTH" ? "Pyth" : ""}`;
+  const [contractExchangeRate, ...marketInfos] = await Promise.all([
+    cauldron
+      ? await (publicClient as any).readContract({
+        ...cauldron,
+        functionName: "exchangeRate",
+      }) as Promise<bigint> : undefined,
+    await publicClient.multicall({
+      contracts: configs.map((config) => {
+        const methodName =
+          config.version === 2
+            ? `getMarketInfoCauldronV2${config.cauldronSettings.oracleInfo?.kind === "PYTH" ? "Pyth" : ""}` as const
+            : `getMarketInfoCauldronV3${config.cauldronSettings.oracleInfo?.kind === "PYTH" ? "Pyth" : ""}` as const;
 
-      return {
-        address: lensAddress,
-        abi: lensAbi,
-        functionName: methodName,
-        args: [config.contract.address],
-      };
-    }),
-  }) as Promise<Array<{ result: Omit<MarketInfo | PythMarketInfo, "kind"> }>>).then((responses) =>
-    responses.map(({ result }, index) => ({
-      kind: configs[index].cauldronSettings.oracleInfo?.kind === "PYTH" ? "PYTH" : "REGULAR",
-      ...result,
-    }) as MarketInfo | PythMarketInfo)
-  );
-
-  const contractExchangeRate: bigint | null = cauldron
-    ? await publicClient.readContract({
-      ...cauldron,
-      functionName: "exchangeRate",
-    }).catch((error: ReadContractErrorType) => {
-      // Pyth cauldrons will likely revert and throw
-      // catch this as it's expected
-      if (error.cause.name !== "ContractFunctionRevertedError") {
-        throw error;
-      }
-      return null;
+        return {
+          address: lensAddress,
+          abi: lensAbi,
+          functionName: methodName,
+          args: [config.contract.address],
+        } as const;
+      }),
+      allowFailure: false,
     })
-    : null;
+  ]);
 
   return Promise.all(marketInfos.map(async (marketInfo, index: number) => {
-    const localInterest: number | undefined = configs[index].interest;
-
-    const updatePrice = marketInfo.kind === "PYTH"
-      ? false
-      : (
-        contractExchangeRate
-          ? !BigNumber.from(contractExchangeRate).eq(marketInfo.oracleExchangeRate)
-          : false
-      );
-
-    let oracleExchangeRate;
-    let collateralPrice;
-    let tvl;
-    switch (marketInfo.kind) {
-      case "REGULAR":
-        oracleExchangeRate = marketInfo.oracleExchangeRate;
-        collateralPrice = marketInfo.collateralPrice;
-        tvl = marketInfo.totalCollateral.value;
-        break;
-      case "PYTH": {
-        const config = configs[index];
-        const feedData = await getPythFeedData([configs[index].cauldronSettings.oracleInfo!.feedId]);
-        oracleExchangeRate = feedData.parsed[0].price.price;
-        const wrapper = config.cauldronSettings.oracleInfo!.wrapper;
-        switch (wrapper?.kind) {
-          case "INVERSE":
-            oracleExchangeRate = wrapper.decimalScale / oracleExchangeRate;
-            break;
-          case "ERC4626": {
-            const assets = await (publicClient as PublicClient).readContract({
-              abi: erc4626Abi,
-              address: wrapper.vault,
-              functionName: "convertToAssets",
-              args: [oracleExchangeRate],
-            });
-            oracleExchangeRate = wrapper.decimalScale / assets;
-            break;
-          }
-        }
-        collateralPrice = 10n ** (2n * BigInt(config.collateralInfo.decimals)) / oracleExchangeRate;
-        tvl = (marketInfo.totalCollateral.amount * 10n ** 18n) / oracleExchangeRate;
-        break;
-      }
+    const config = configs[index];
+    switch (config.cauldronSettings.oracleInfo?.kind) {
+      case "PYTH":
+        return await getPythMainParams(publicClient, config, marketInfo as any as PythMarketInfo);
+      case undefined:
+        return getRegularMainParams(config, marketInfo as any as MarketInfo, contractExchangeRate);
     }
-
-    const interest = localInterest
-      ? localInterest
-      : Number(marketInfo.interestPerYear) / 100;
-
-    return {
-      borrowFee: Number(marketInfo.borrowFee) / 100,
-      interest,
-      liquidationFee: Number(marketInfo.liquidationFee) / 100,
-      collateralPrice: BigNumber.from(collateralPrice),
-      mimLeftToBorrow: BigNumber.from(marketInfo.marketMaxBorrow),
-      maximumCollateralRatio: BigNumber.from(marketInfo.maximumCollateralRatio),
-      oracleExchangeRate: BigNumber.from(oracleExchangeRate),
-      totalBorrowed: BigNumber.from(marketInfo.totalBorrowed),
-      tvl: BigNumber.from(tvl),
-      userMaxBorrow: BigNumber.from(marketInfo.userMaxBorrow),
-      updatePrice,
-      alternativeData: {
-        collateralPrice,
-        mimLeftToBorrow: marketInfo.marketMaxBorrow,
-        maximumCollateralRatio: marketInfo.maximumCollateralRatio,
-        oracleExchangeRate,
-        totalBorrowed: marketInfo.totalBorrowed,
-        tvl,
-        userMaxBorrow: marketInfo.userMaxBorrow,
-      },
-    };
   }));
 };
+
+const getRegularMainParams = (config: CauldronConfig, marketInfo: MarketInfo, contractExchangeRate?: bigint): MainParams => {
+  const updatePrice = contractExchangeRate
+    ? !BigNumber.from(contractExchangeRate).eq(marketInfo.oracleExchangeRate)
+    : false;
+  return {
+    borrowFee: Number(marketInfo.borrowFee) / 100,
+    interest: config.interest ?? Number(marketInfo.interestPerYear) / 100,
+    liquidationFee: Number(marketInfo.liquidationFee) / 100,
+    collateralPrice: BigNumber.from(marketInfo.collateralPrice),
+    mimLeftToBorrow: BigNumber.from(marketInfo.marketMaxBorrow),
+    maximumCollateralRatio: BigNumber.from(marketInfo.maximumCollateralRatio),
+    oracleExchangeRate: BigNumber.from(marketInfo.oracleExchangeRate),
+    totalBorrowed: BigNumber.from(marketInfo.totalBorrowed),
+    tvl: BigNumber.from(marketInfo.totalCollateral.value),
+    userMaxBorrow: BigNumber.from(marketInfo.userMaxBorrow),
+    updatePrice,
+    alternativeData: {
+      collateralPrice: marketInfo.collateralPrice,
+      mimLeftToBorrow: marketInfo.marketMaxBorrow,
+      maximumCollateralRatio: marketInfo.maximumCollateralRatio,
+      oracleExchangeRate: marketInfo.oracleExchangeRate,
+      totalBorrowed: marketInfo.totalBorrowed,
+      tvl: marketInfo.totalCollateral.value,
+      userMaxBorrow: marketInfo.userMaxBorrow,
+    },
+  };
+};
+
+const getPythMainParams = async (publicClient: PublicClient, config: CauldronConfig, marketInfo: PythMarketInfo): Promise<MainParams> => {
+  const feedData = await getPythFeedData([config.cauldronSettings.oracleInfo!.feedId]);
+  let oracleExchangeRate = feedData.parsed[0].price.price;
+  const wrapper = config.cauldronSettings.oracleInfo!.wrapper;
+  switch (wrapper?.kind) {
+    case "INVERSE":
+      oracleExchangeRate = wrapper.decimalScale / oracleExchangeRate;
+      break;
+    case "ERC4626": {
+      const assets = await (publicClient as PublicClient).readContract({
+        abi: erc4626Abi,
+        address: wrapper.vault,
+        functionName: "convertToAssets",
+        args: [oracleExchangeRate],
+      });
+      oracleExchangeRate = wrapper.decimalScale / assets;
+    }
+  }
+  const collateralPrice = 10n ** (2n * BigInt(config.collateralInfo.decimals)) / oracleExchangeRate;
+  const tvl = (marketInfo.totalCollateral.amount * 10n ** 18n) / oracleExchangeRate;
+
+  return {
+    borrowFee: Number(marketInfo.borrowFee) / 100,
+    interest: config.interest ?? Number(marketInfo.interestPerYear) / 100,
+    liquidationFee: Number(marketInfo.liquidationFee) / 100,
+    collateralPrice: BigNumber.from(collateralPrice),
+    mimLeftToBorrow: BigNumber.from(marketInfo.marketMaxBorrow),
+    maximumCollateralRatio: BigNumber.from(marketInfo.maximumCollateralRatio),
+    oracleExchangeRate: BigNumber.from(oracleExchangeRate),
+    totalBorrowed: BigNumber.from(marketInfo.totalBorrowed),
+    tvl: BigNumber.from(tvl),
+    userMaxBorrow: BigNumber.from(marketInfo.userMaxBorrow),
+    updatePrice: false,
+    alternativeData: {
+      collateralPrice,
+      mimLeftToBorrow: marketInfo.marketMaxBorrow,
+      maximumCollateralRatio: marketInfo.maximumCollateralRatio,
+      oracleExchangeRate,
+      totalBorrowed: marketInfo.totalBorrowed,
+      tvl,
+      userMaxBorrow: marketInfo.userMaxBorrow,
+    },
+  }
+}
