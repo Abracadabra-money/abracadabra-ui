a03e922eb5eb6a7584dce313bb368afac5c2f8f2
0xmDreamy <0xmDreamy@proton.me>
2025-02-20 19:58:36 +0100

    🐞 Fix repay cauldron

--- src/helpers/approval.ts
+++ src/helpers/approval.ts
@@ -4,35 +4,8 @@ import {
   waitForTransactionReceiptHelper,
 } from "@/helpers/walletClienHelper";
 import type { Address } from "viem";
-import type { Contract } from "ethers";
 import type { ContractInfo } from "@/types/global";
 import { MAX_ALLOWANCE_VALUE } from "@/constants/global";
-
-const APPROVE_GAS_LIMIT: number = 1000;
-
-export const approveToken = async (
-  contract: Contract,
-  spender: string
-): Promise<Boolean> => {
-  try {
-    const estimateGas = await contract.estimateGas.approve(
-      spender,
-      MAX_ALLOWANCE_VALUE
-    );
-
-    const gasLimit = APPROVE_GAS_LIMIT + +estimateGas.toString();
-
-    const tx = await contract.approve(spender, MAX_ALLOWANCE_VALUE, {
-      gasLimit,
-    });
-
-    await tx.wait();
-    return true;
-  } catch (error) {
-    console.log("Approve Token:", error);
-    return false;
-  }
-};
 
 export const approveTokenViem = async (
   contract: ContractInfo,
