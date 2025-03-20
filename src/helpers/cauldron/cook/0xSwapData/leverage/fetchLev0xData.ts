4847dce42d9e8a2aa752b8aaed3de10430c3bb45
Andrey <Andreysk8er@ukr.net>
2025-03-20 09:36:19 +0200

    updated fetchLev0xData helper

--- src/helpers/cauldron/cook/0xSwapData/leverage/fetchLev0xData.ts
+++ src/helpers/cauldron/cook/0xSwapData/leverage/fetchLev0xData.ts
@@ -1,8 +1,7 @@
-import { swap0xRequest, swap0xRequestV2 } from "@/helpers/0x";
-
-import type { CauldronInfo } from "@/helpers/cauldron/types";
-import type { BigNumber } from "ethers";
 import type { Address } from "viem";
+import type { BigNumber } from "ethers";
+import type { CauldronInfo } from "@/helpers/cauldron/types";
+import { swap0xRequest, swap0xRequestV2 } from "@/helpers/0x";
 
 export const fetchLev0xData = async (
   cauldronObject: CauldronInfo,
@@ -31,7 +30,7 @@ export const fetchLev0xDataV2 = async (
   cauldronObject: CauldronInfo,
   amount: BigNumber,
   slipage: number,
-  buyToken: Address
+  buyToken?: Address
 ) => {
   // @ts-ignore
   const { collateral, mim, leverageSwapper } = cauldronObject.contracts;
