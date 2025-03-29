670f7c0f33fdcdd56d17ec1b6fb04e1aaeeca188
0xmDreamy <124707942+0xmDreamy@users.noreply.github.com>
2025-03-29 10:52:43 +0100

    🐞 Disable bartio (#559)

--- src/configs/cauldrons/index.ts
+++ src/configs/cauldrons/index.ts
@@ -1,11 +1,11 @@
 import ethereumCauldrons from "@/configs/cauldrons/ethereumCauldrons";
-import fantomCauldrons from "@/configs/cauldrons/fantomCauldrons";
+// import fantomCauldrons from "@/configs/cauldrons/fantomCauldrons";
 import avalancheCauldrons from "@/configs/cauldrons/avalancheCauldrons";
 import arbitrumCauldrons from "@/configs/cauldrons/arbitrumCauldrons";
 import bscCauldrons from "@/configs/cauldrons/bscCauldrons";
 import optimismCauldrons from "@/configs/cauldrons/optimismCauldrons";
 import kavaCauldrons from "@/configs/cauldrons/kavaCauldrons";
-import beraBartioCauldrons from "@/configs/cauldrons/beraBartioCauldrons";
+// import beraBartioCauldrons from "@/configs/cauldrons/beraBartioCauldrons";
 import blastCauldrons from "@/configs/cauldrons/blastCauldrons";
 
 export default [
@@ -16,6 +16,6 @@ export default [
   ...bscCauldrons,
   ...optimismCauldrons,
   ...kavaCauldrons,
-  ...beraBartioCauldrons,
+  // ...beraBartioCauldrons,
   ...blastCauldrons,
 ];
