88151f47c0a242bd9ac2d5748ba08c87dd159375
0xhardrefresh <98540076+0xhardrefresh@users.noreply.github.com>
2025-03-31 18:24:14 +0300

    comment bartio chain config (#561)

--- src/helpers/chains/configs/index.ts
+++ src/helpers/chains/configs/index.ts
@@ -1,7 +1,7 @@
 import { arbitrumConfig } from "@/helpers/chains/configs/arbitrum";
 import { avalancheConfig } from "@/helpers/chains/configs/avalanche";
 import { baseConfig } from "@/helpers/chains/configs/base";
-import { beraBartioConfig } from "@/helpers/chains/configs/beraBartio";
+// import { beraBartioConfig } from "@/helpers/chains/configs/beraBartio";
 import { binanceConfig } from "@/helpers/chains/configs/binance";
 import { blastConfig } from "@/helpers/chains/configs/blast";
 import { fantomConfig } from "@/helpers/chains/configs/fantom";
@@ -27,5 +27,5 @@ export const chainsConfigs = [
   moonriverConfig,
   baseConfig,
   lineaConfig,
-  beraBartioConfig,
+  // beraBartioConfig,
 ];
