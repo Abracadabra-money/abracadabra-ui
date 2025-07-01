bc5d220a28a712cc1d538f9ab32ed3889bf82ced
5450L <susalafamilia@gmail.com>
2025-07-01 13:42:49 +0300

    migrationHelpers utils

--- src/helpers/migrationHelpers/utils.ts
+++ src/helpers/migrationHelpers/utils.ts
@@ -1,14 +1,17 @@
 // @/helpers/cauldron/utils.ts
 
-import { parseUnits } from "viem";
+import { parseUnits, formatUnits } from "viem";
 import { applySlippageToMinOutBigInt } from "../gm/applySlippageToMinOut";
+import type { AlternativePositionHealth } from "../cauldron/types";
 
 const MIM_DECIMALS = 18;
 const COLATERIZATION_PRECISION = 5;
 const BORROW_OPENING_FEE_PRECISION = 5;
 export const PERCENT_PRESITION = 2;
 
-type SwapAmounts = {
+const FENCING_AGAINST_LIQUIDATION = parseUnits("1", PERCENT_PRESITION); // 1% of mcr
+
+type SwapAmountsBigint = {
   amountFrom: bigint;
   amountToMin: bigint;
 };
@@ -18,7 +21,7 @@ export function expandDecimals(n: bigint, decimals: number): bigint {
   return n * BigInt(Math.pow(10, decimals));
 }
 
-export const getLiquidationPrice = (
+export const getLiquidationPriceBigint = (
   borrowAmount: bigint,
   collateralAmount: bigint,
   mcr: number,
@@ -35,30 +38,134 @@ export const getLiquidationPrice = (
   return scaledBorrowAmount / collateralAmount / colaterizationRate;
 };
 
-// getAlternativeLiquidationPrice
+// @/helpers/cauldron/utils.ts
+export const getMaxToBorrowBigint = (
+  collateralAmount: bigint,
+  userBorrowAmount: bigint,
+  mcr: bigint,
+  oracleExchangeRate: bigint
+): bigint => {
+  const collateralInMim =
+    expandDecimals(collateralAmount, MIM_DECIMALS) / oracleExchangeRate;
 
-// getMaxToBorrow
+  const maxToBorrow =
+    (collateralInMim * (mcr - FENCING_AGAINST_LIQUIDATION)) /
+    (100n * parseUnits("1", PERCENT_PRESITION));
 
-// getUserLtv
+  const maxToBorrowLeft = maxToBorrow - userBorrowAmount;
+  return maxToBorrowLeft < 0n ? 0n : maxToBorrowLeft;
+};
 
-// getMimToBorrowByLtv
+export const getUserLtvBigint = (
+  collateralAmount: bigint,
+  userBorrowAmount: bigint,
+  oracleExchangeRate: bigint
+): bigint => {
+  if (collateralAmount === 0n) return 0n;
 
-// getMaxCollateralToRemove
+  const collateralInMim =
+    expandDecimals(collateralAmount, MIM_DECIMALS) / oracleExchangeRate;
 
-// getPositionHealth
+  const ltv =
+    (expandDecimals(userBorrowAmount, PERCENT_PRESITION) * 100n) /
+    collateralInMim;
 
-// getAlternativePositionHealth
+  return ltv;
+};
 
-// getHealthStatus
+export const getMimToBorrowByLtvBigint = (
+  ltv: bigint,
+  mcr: bigint,
+  collateralAmount: bigint,
+  userBorrowAmount: bigint,
+  oracleExchangeRate: bigint
+): bigint => {
+  if (ltv > mcr) return 0n;
 
-// getAlternativeHealthStatus
+  const currentLtv = getUserLtvBigint(
+    collateralAmount,
+    userBorrowAmount,
+    oracleExchangeRate
+  );
+
+  if (ltv <= currentLtv) return 0n;
+
+  const collateralInMim =
+    expandDecimals(collateralAmount, MIM_DECIMALS) / oracleExchangeRate;
+
+  const leftToBorrow = getMaxToBorrowBigint(
+    collateralAmount,
+    userBorrowAmount,
+    mcr,
+    oracleExchangeRate
+  );
+  const mimPerPercent = collateralInMim / 100n;
+
+  const mimToBorrow =
+    (mimPerPercent * (ltv - currentLtv)) / parseUnits("1", PERCENT_PRESITION);
+
+  return mimToBorrow > leftToBorrow ? leftToBorrow : mimToBorrow;
+};
+
+export const getMaxCollateralToRemoveBigint = (
+  collateralAmount: bigint,
+  userBorrowAmount: bigint,
+  mcr: bigint,
+  oracleExchangeRate: bigint
+): bigint => {
+  if (userBorrowAmount === 0n) return collateralAmount;
+
+  const currentLtv = getUserLtvBigint(
+    collateralAmount,
+    userBorrowAmount,
+    oracleExchangeRate
+  );
+
+  const minCollateralAmount =
+    (currentLtv * collateralAmount) / (mcr - FENCING_AGAINST_LIQUIDATION);
+
+  const maxToRemoveLeft = collateralAmount - minCollateralAmount;
+
+  const maxToRemove = maxToRemoveLeft < 0n ? 0n : maxToRemoveLeft;
+
+  return maxToRemove > collateralAmount ? collateralAmount : maxToRemove;
+};
+
+export const getPositionHealthBigint = (
+  liquidationPrice: bigint,
+  oracleExchangeRate: bigint,
+  collateralDecimals: number
+): AlternativePositionHealth => {
+  if (oracleExchangeRate === 0n) return { percent: 0n, status: "safe" };
+
+  const collateralPrice =
+    parseUnits("1", 18 + collateralDecimals) / oracleExchangeRate;
+
+  const percent =
+    (liquidationPrice * parseUnits("1", PERCENT_PRESITION) * 100n) /
+    collateralPrice;
+
+  const status = getHealthStatusBigint(percent);
+
+  return { percent, status };
+};
+
+const getHealthStatusBigint = (
+  riskPercent: bigint
+): "safe" | "medium" | "high" => {
+  const percent = Number(formatUnits(riskPercent, PERCENT_PRESITION));
+
+  if (percent >= 0 && percent <= 70) return "safe";
+  if (percent > 70 && percent <= 90) return "medium";
+  return "high";
+};
 
 export const getLeverageAmounts = (
   collateralAmount: bigint,
   leverageMultiplyer: bigint,
   slippage: bigint,
   oracleExchangeRate: bigint
-): SwapAmounts => {
+): SwapAmountsBigint => {
   if (collateralAmount === 0n) return { amountFrom: 0n, amountToMin: 0n };
 
   const collateralToSwap =
@@ -75,10 +182,47 @@ export const getLeverageAmounts = (
   };
 };
 
-// getDeleverageAmounts
+export const getDeleverageAmountsBigint = (
+  mimToRepayAmount: bigint,
+  slippage: bigint, // 1e2
+  oracleExchangeRate: bigint
+): SwapAmountsBigint => {
+  if (mimToRepayAmount === 0n) return { amountFrom: 0n, amountToMin: 0n };
 
-// applyBorrowFee
+  const collateralToSwapMin =
+    (mimToRepayAmount * oracleExchangeRate) / expandDecimals(1n, MIM_DECIMALS);
 
-// alternativeApplyBorrowFee
+  const additionalSlippageAmount =
+    (collateralToSwapMin * slippage) / expandDecimals(100n, PERCENT_PRESITION);
 
-// applyTokenWrapperRate
+  const collateralToSwapAmount = collateralToSwapMin + additionalSlippageAmount;
+
+  return {
+    amountFrom: collateralToSwapAmount, // collateral amount to remove & swap
+    amountToMin: mimToRepayAmount, // min expected MIM amount
+  };
+};
+
+export const applyBorrowFeeBigint = (
+  borrowAmount: bigint,
+  borrowOpeningFee: number // 1% === 1000
+): bigint => {
+  if (borrowAmount === 0n) return 0n;
+
+  const fee =
+    (borrowAmount * parseUnits(borrowOpeningFee.toString(), 0)) /
+    parseUnits("1", BORROW_OPENING_FEE_PRECISION);
+
+  return borrowAmount + fee;
+};
+
+// NOTICE: we expect unwrapped token decimals = wrapped decimals
+export const applyTokenWrapperRateBigint = (
+  unwrappedTokenAmount: bigint,
+  tokensRate: bigint,
+  unwrappedTokenDecimals: number
+): bigint => {
+  return (
+    expandDecimals(unwrappedTokenAmount, unwrappedTokenDecimals) / tokensRate
+  );
+};
